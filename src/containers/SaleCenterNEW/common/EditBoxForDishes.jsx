4a8545b944eb0c336236b577d9791e8c60bd5f84
wangsen <13683155318@qq.com>
2017-12-28 18:16:38 +0800

    推荐菜check2

--- src/containers/SaleCenterNEW/common/EditBoxForDishes.jsx
+++ src/containers/SaleCenterNEW/common/EditBoxForDishes.jsx
@@ -122,11 +122,12 @@ class EditBoxForDishes extends React.Component {
             })
         }
         if (this.props.promotionDetailInfo.getIn(['$promotionDetail', 'priceLst']) !==
-            nextProps.promotionDetailInfo.getIn(['$promotionDetail', 'priceLst'])
+            nextProps.promotionDetailInfo.getIn(['$promotionDetail', 'priceLst']) ||
+            !Immutable.is(Immutable.fromJS(this.props.value), Immutable.fromJS(nextProps.value))
         ) {
             const _priceLst = nextProps.promotionDetailInfo.getIn(['$promotionDetail', 'priceLst']).toJS();
             this.setState({
-                priceLst: this.props.type === 'RECOMMEND_FOOD' ? this.props.value : _priceLst,
+                priceLst: nextProps.type === 'RECOMMEND_FOOD' ? nextProps.value : _priceLst,
             }, () => {
                 this.initialState(this.state.priceLst, this.state.foodCategoryCollection);
             })
