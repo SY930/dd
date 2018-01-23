e26cc2da6594c5851395b43cfc4c6133dd9f8425
AmBeta <cyandrewchen@gmail.com>
2018-01-23 17:23:27 +0800

    业务包独立构建相关修改

--- src/index.jsx
+++ src/index.jsx
@@ -2,6 +2,6 @@ import registerPackage from '@hualala/platform-base'
 
 import * as entryCodes from './constants/entryCodes'
 
-const { registeEntryCode } = registerPackage('sale')
+const { registeEntryCode } = registerPackage('sale', process.env.JS_VERSION)
 
 export default registeEntryCode(entryCodes, completed => import('./components').then(completed))
