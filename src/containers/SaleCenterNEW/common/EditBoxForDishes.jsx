37e639502a81748b1b9eb0a79b5d4c8b541be0c0
wangsen <13683155318@qq.com>
2018-01-29 10:54:47 +0800

    debug菜品框

--- src/containers/SaleCenterNEW/common/EditBoxForDishes.jsx
+++ src/containers/SaleCenterNEW/common/EditBoxForDishes.jsx
@@ -123,13 +123,22 @@ class EditBoxForDishes extends React.Component {
         }
         if (
             this.props.promotionDetailInfo.getIn(['$promotionDetail', 'priceLst']) !==
-            nextProps.promotionDetailInfo.getIn(['$promotionDetail', 'priceLst']) ||
-            !Immutable.is(Immutable.fromJS(this.props.value), Immutable.fromJS(nextProps.value))
+            nextProps.promotionDetailInfo.getIn(['$promotionDetail', 'priceLst'])
         ) {
             let _priceLst = nextProps.promotionDetailInfo.getIn(['$promotionDetail', 'priceLst']);
             _priceLst = _priceLst ? _priceLst.toJS() : [];
             this.setState({
-                priceLst: nextProps.type === 'RECOMMEND_FOOD' || nextProps.type === 'FOOD_PAY_MORE_THEN_UPGRADE' ? nextProps.value : _priceLst,
+                priceLst: nextProps.type === 'RECOMMEND_FOOD' ? nextProps.value : _priceLst,
+            }, () => {
+                this.initialState(this.state.priceLst, this.state.foodCategoryCollection);
+            })
+        }
+        if (
+            nextProps.type === 'FOOD_PAY_MORE_THEN_UPGRADE' &&
+            !Immutable.is(Immutable.fromJS(this.props.value), Immutable.fromJS(nextProps.value))
+        ) {
+            this.setState({
+                priceLst: nextProps.value
             }, () => {
                 this.initialState(this.state.priceLst, this.state.foodCategoryCollection);
             })
