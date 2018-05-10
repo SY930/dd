43b028f0f469fa08d4741469122664808102739a
wuhao <wuhao@hualala.com>
2018-05-10 14:03:42 +0800

    前端增加特价菜同一菜品使用数量限制,待与后端对接

--- src/containers/SaleCenterNEW/special/specialDetailInfo.jsx
+++ src/containers/SaleCenterNEW/special/specialDetailInfo.jsx
@@ -33,6 +33,7 @@ import {
     fetchFoodCategoryInfoAC,
     fetchFoodMenuInfoAC,
 } from '../../../redux/actions/saleCenterNEW/promotionDetailInfo.action';
+import PriceInput from "../common/PriceInput";
 
 class SpecialDetailInfo extends React.Component {
     constructor(props) {
@@ -45,10 +46,14 @@ class SpecialDetailInfo extends React.Component {
             foodSelections: new Set(),
             foodCurrentSelections: [],
             data: [],
+            amountLimit: 1, // 特价菜同一菜品使用数量限制, 默认为1份, int, > 0
+            isLimited: 0, // 0 为不限制使用数量, 1为限制
         };
 
         this.renderAdvancedSettingButton = this.renderAdvancedSettingButton.bind(this);
         this.onChangeClick = this.onChangeClick.bind(this);
+        this.handleIsLimitedChange = this.handleIsLimitedChange.bind(this);
+        this.handleAmountLimitChange = this.handleAmountLimitChange.bind(this);
         this.dishesChange = this.dishesChange.bind(this);
     }
 
@@ -57,8 +62,7 @@ class SpecialDetailInfo extends React.Component {
             finish: this.handleSubmit,
         });
         const _categoryOrDish = this.props.promotionDetailInfo.getIn(['$promotionDetail', 'categoryOrDish']);
-        let { display } = this.state;
-        display = !this.props.isNew;
+        const display = !this.props.isNew;
         this.setState({
             display,
             targetScope: _categoryOrDish,
@@ -117,6 +121,14 @@ class SpecialDetailInfo extends React.Component {
         )
     }
 
+    handleIsLimitedChange(value) {
+        this.setState({isLimited: value})
+    }
+
+    handleAmountLimitChange(value) {
+        // console.log(value);
+        this.setState({amountLimit: value.number})
+    }
 
     render() {
         return (
@@ -125,6 +137,50 @@ class SpecialDetailInfo extends React.Component {
                     <SpecialDishesTable
                         onChange={this.dishesChange}
                     />
+                    {/*<div>
+                        <Select onChange={this.handleIsLimitedChange} value={isLimited}>
+                            <Option key="0" value={0}>不加价</Option>
+                            <Option key="1" value={1}>加价</Option>
+                        </Select>
+                    </div>*/}
+                    {/*<FormItem
+                        className={styles.FormItemStyle}
+                        label='单笔订单同一菜品最多使用数量限制'
+                        labelCol={{ span: 7 }}
+                        wrapperCol={{ span: 13 }}
+                    >*/}
+                    <div style={{height: '50px', marginTop: '8px'}} className={styles.flexContainer}>
+                        <div style={{lineHeight: '28px', marginRight: '14px'}}>{'单笔订单同一菜品最多使用数量限制'}</div>
+                        <div style={{width: '300px'}}>
+
+                            <Col  span={this.state.isLimited == 0 ? 24 : 8}>
+                                <Select onChange={this.handleIsLimitedChange} value={this.state.isLimited}>
+                                    <Option key="0" value={0}>不限制</Option>
+                                    <Option key="1" value={1}>限制</Option>
+                                </Select>
+                            </Col>
+                            {
+                                this.state.isLimited == 1 ?
+                                    <Col span={this.state.isLimited == 0 ? 0 : 16}>
+                                        <FormItem
+                                            style={{ marginTop: -6 }}
+                                            validateStatus={this.state.amountLimit > 0 ? 'success' : 'error'}
+                                            help={this.state.amountLimit > 0 ? null : '必须大于0'}
+                                        >
+                                            <PriceInput
+                                                addonAfter={'份'}
+                                                value={{ number: this.state.amountLimit }}
+                                                defaultValue={{ number: this.state.amountLimit }}
+                                                onChange={this.handleAmountLimitChange}
+                                                modal="int"
+                                            />
+                                        </FormItem>
+                                    </Col> : null
+                            }
+                        </div>
+                </div>
+
+                    {/*</FormItem>*/}
                     {this.renderAdvancedSettingButton()}
                     {this.state.display ? <AdvancedPromotionDetailSetting payLimit={false} /> : null}
                 </Form>
