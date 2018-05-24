6efa080f8decd0a18e34ee868c6709854ab6a91e
wuhao <wuhao@hualala.com>
2018-05-24 15:56:08 +0800

    哦, 就我最着急

--- src/containers/CrmDataValidator/ThreeStepsValidator.jsx
+++ src/containers/CrmDataValidator/ThreeStepsValidator.jsx
@@ -5,7 +5,7 @@ import style from '../SaleCenterNEW/ActivityPage.less';
 import ownStyle from './Validator.less';
 import { connect } from 'react-redux';
 import { Steps, Button, Form, Select, Upload, Icon, message, Col } from 'antd';
-import ENV from "../../helpers/env";
+import { axiosData } from '../../helpers/util';
 const Option = Select.Option;
 const OptGroup = Select.OptGroup;
 
@@ -28,20 +28,27 @@ class ThreeStepsValidator extends React.Component {
             dataType: '1',
             isBusyTime: false, // 忙时不允许发起校验请求
             isLoading: false, // 校验请求loading
-            adjustmentMethod: '1'
+            adjustmentMethod: '1',
+            fileList: []
         };
         this.intervalId = null;
         this.handleAdjustmentMethodChange = this.handleAdjustmentMethodChange.bind(this);
         this.handleTypeChange = this.handleTypeChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
         this.steps = null;
+        this.importID = null;
     }
 
     componentDidMount() {
-        /*this.intervalId = window.setInterval(() => {
+        this.intervalId = window.setInterval(() => {
             this.setState({
-                isBusy: moment().seconds()%2 === 0
+                isBusy: false
             })
-        }, 500)*/
+        }, 500);
+        this.importID = localStorage.getItem('_crm_import_id');
+        if (this.importID) {
+            this.setState({current: 1});
+        }
     }
 
     componentWillUnmount() {
@@ -56,34 +63,80 @@ class ThreeStepsValidator extends React.Component {
         this.setState({adjustmentMethod: value});
     }
 
+    handleSubmit() {
+        // 文件list为空
+        if (this.state.fileList.length === 0) {
+            message.warning('请至少上传一个excel文件');
+            return;
+        }
+        const fileList = this.state.fileList;
+        const isUploadingFinished = fileList.every(file => file.status === 'done');
+        if (!isUploadingFinished) {
+            message.warning('还有文件正在上传中');
+            return;
+        }
+        let fileLocationStr;
+        try {
+            const fileLocationUrlArr = fileList.map(file => file.response.data.url);
+            if (fileLocationUrlArr.every(url => !!url)) {
+                fileLocationStr = fileLocationUrlArr.join(',');
+            } else {
+                message.warning('有部分文件未能上传成功,请重新上传或刷新重试');
+                return;
+            }
+        } catch (e) {
+            message.warning('有部分文件未能上传成功,请重新上传或刷新重试');
+            return;
+        }
+        const importID = this.generateImportID();
+        const reqParams = {
+            groupID: this.props.user.accountInfo.groupID,
+            groupName: this.props.user.accountInfo.groupName,
+            sourceFilePath: fileLocationStr,
+            importID,
+            crmVersion: '21' // 会员系统版本: 10(老系统) 21(多卡类型会员系统)
+        };
+        /*axiosData('crmImport/crmImportService_doImport.ajax', reqParams, {}, undefined, 'HTTP_SERVICE_URL_SHOPCENTER')
+            .then(res => {
+                console.log(res);
+            }, err => {
+                console.log(err);
+            });*/
+        localStorage.setItem('_crm_import_id', importID);
+        this.setState({current: 1});
+    }
+
+    generateImportID() {
+        return `${this.props.user.accountInfo.groupID}_${this.state.fileList.length}_${Date.now()}`;
+    }
+
     renderUploadButton() {
         const props = {
             name: 'file',
             accept: '.xls,.xlsx', // 上传的是excel 文件
             action: '/api/shopcenter/upload',
             onChange: (info) => {
-                console.log('new info', info);
                 const status = info.file.status;
-                const fileList = info.fileList;
-                /*this.setState({
-                    giftImagePath,
-                })
-                if (status !== 'uploading') {
-                    // console.log(info.file, info.fileList);
-                }*/
-
-                if (status === 'done') {
+                let fileList = info.fileList;
+                fileList = fileList.filter((file) => {
+                    if (file.response) {
+                        return file.response.status !== 'error';
+                    }
+                    if (status) {
+                        return status !== 'error'
+                    }
+                    return true;
+                });
+                this.setState({ fileList });
+                /*if (status === 'done') {
                     message.success(`${info.file.name} 上传成功`);
-                    this.setState({
-                        imageUrl: `${ENV.FILE_RESOURCE_DOMAIN}/${info.file.response.url}`,
-                    })
                 } else if (status === 'error') {
                     message.error(`${info.file.name} 上传失败`);
-                }
+                }*/
             },
         };
         return (
-            <Upload {...props}>
+            <Upload {...props} fileList={this.state.fileList}>
                 <Button style={{width: '185px', textAlign: 'left', paddingLeft: '7px'}}>
                     上传Excel附件
                 </Button>
@@ -166,13 +219,42 @@ class ThreeStepsValidator extends React.Component {
             {
                 title: '审核进度',
                 content: (
-                    <div>{`数据正在审核,请稍后查看`}</div>
+                    <div className="layoutsContent">
+                        <div style={{width: '200px', margin: '0 auto', textAlign: 'center'}}>{`数据正在审核,请稍后查看`}</div>
+                    </div>
                 ),
             },
             {
                 title: '校验状态',
                 content: (
-                    <div>{this.props.user.shopID}</div>
+                    <div>
+                        <Form className={style.FormStyle}>
+                            <FormItem
+                                label="集团名称"
+                                className={style.FormItemStyle}
+                                labelCol={{ span: 11 }}
+                                wrapperCol={{ span: 13 }}
+                            >
+                                <p>{`${this.props.user.accountInfo.groupShortName}(ID: ${this.props.user.accountInfo.groupID})`}</p>
+                            </FormItem>
+                            <FormItem
+                                label="集团名称"
+                                className={style.FormItemStyle}
+                                labelCol={{ span: 11 }}
+                                wrapperCol={{ span: 13 }}
+                            >
+                                <p>{`${this.props.user.accountInfo.groupShortName}(ID: ${this.props.user.accountInfo.groupID})`}</p>
+                            </FormItem>
+                            <FormItem
+                                label="校验状态"
+                                className={style.FormItemStyle}
+                                labelCol={{ span: 11 }}
+                                wrapperCol={{ span: 13 }}
+                            >
+                                <p>{'数据校验无误'}</p>
+                            </FormItem>
+                        </Form>
+                    </div>
                 ),
             },
         ];
@@ -189,15 +271,15 @@ class ThreeStepsValidator extends React.Component {
                     return (<div key={index} className="stepsContent" style={{ display: 'none' }}>{steps[index].content}</div>);
                 })}
 
-                <div className="progressButton">
+                {this.state.current === 0 && (<div className="progressButton">
                     <Button
                         disabled={this.state.isBusyTime}
                         loading={this.state.isLoading}
                         type="primary"
-                        onClick={() => console.log(moment().hours())}
+                        onClick={this.handleSubmit}
                     >确定
                     </Button>
-                </div>
+                </div>)}
             </div>
         );
     }
