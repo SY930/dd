88fd4d02c0a7203b5c43109fb8cafee25df91a84
wuhao <wuhao@hualala.com>
2018-05-28 17:47:44 +0800

    临时提交

--- src/containers/BasicSettings/index.jsx
+++ src/containers/BasicSettings/index.jsx
@@ -1,47 +1,66 @@
 import React from 'react';
 import registerPage from '../../index';
+import {
+    Row,
+    Button,
+    Col,
+} from 'antd';
 import styles from '../SaleCenterNEW/ActivityPage.less';
 import { SET_MSG_TEMPLATE } from '../../constants/entryCodes';
+import MessageDisplayBox from './MessageDisplayBox'
+
+const mock = new Array(50).fill(1);
 
 @registerPage([SET_MSG_TEMPLATE], {
 })
 class MessageTemplatesPage extends React.Component {
     constructor(props) {
         super(props);
+        this.state = {
+            contentHeight: 800,
+        };
+        this.onWindowResize = this.onWindowResize.bind(this);
+    }
+
+    componentDidMount() {
+        this.onWindowResize();
+        window.addEventListener('resize', this.onWindowResize);
+    }
+
+    componentWillUnmount() {
+        window.removeEventListener('resize', this.onWindowResize);
     }
 
     onWindowResize() {
-        const contentHeight = document.querySelector('.ant-tabs-tabpane-active').offsetHeight - 40;
+        const contentHeight = document.querySelector('.ant-tabs-tabpane-active').offsetHeight - 86;
         this.setState({ contentHeight });
     }
 
     render() {
+        const headerClasses = `layoutsToolLeft ${styles.headerWithBgColor}`;
         return (
-            <Row className="layoutsContainer">
-                <Col span={24} className="layoutsHeader">
-                    <div className="layoutsTool">
-                        <div className="layoutsToolLeft">
-                            <h1>新建基础营销</h1>
-                        </div>
-                        <Button
-                            type="ghost"
-                            icon="plus"
-                            className={styles.jumpToCreate}
-                            onClick={
-                                () => {
-                                    console.log('123');
-                                }
-                            }>新建</Button>
-                    </div>
-                </Col>
-                <Col span={24} className="layoutsLineBlock"></Col>
-                <Col span={24} className="layoutsContent" style={{ overflow: 'auto', height: this.state.contentHeight || 800 }}>
-                    <ul>
-                        {this.renderActivityButtons()}
-                    </ul>
-                    {this.state.modal1Visible ? this.renderModal() : null}
-                </Col>
-            </Row>
+        <div style={{backgroundColor: '#F3F3F3'}} className="layoutsContainer">
+            <div className="layoutsTool" style={{height: '80px'}}>
+                <div className={headerClasses} style={{lineHeight: '80px'}}>
+                    <span style={{lineHeight: '80px'}} className={styles.customHeader}>短信模板</span>
+                    <Button
+                        type="ghost"
+                        icon="plus"
+                        className={styles.jumpToCreate}
+                        onClick={
+                            () => {
+                                console.log('123');
+                            }
+                        }>新建</Button>
+                </div>
+                <div style={{height: this.state.contentHeight}} className={styles.scrollableMessageContainer}>
+                    {mock.map((item, index) => {
+                        return <MessageDisplayBox key={index}/>;
+                    })}
+                </div>
+
+            </div>
+        </div>
         );
     }
 }
