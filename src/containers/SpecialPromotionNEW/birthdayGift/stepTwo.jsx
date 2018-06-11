73a05fac5d7390a6f71d0feb75b9fb39c7e208b9
wuhao <wuhao@hualala.com>
2018-06-11 18:31:07 +0800

    会员群体初步提交

--- src/containers/SpecialPromotionNEW/birthdayGift/stepTwo.jsx
+++ src/containers/SpecialPromotionNEW/birthdayGift/stepTwo.jsx
@@ -10,31 +10,38 @@
 
 import React from 'react';
 import { connect } from 'react-redux';
-import { Form } from 'antd';
+import { Form, Select } from 'antd';
+import styles from '../../SaleCenterNEW/ActivityPage.less';
 import { saleCenterSetSpecialBasicInfoAC } from '../../../redux/actions/saleCenterNEW/specialPromotion.action'
 // import styles from '../../SaleCenterNEW/ActivityPage.less';
 import SendMsgInfo from '../common/SendMsgInfo';
 import CardLevel from '../common/CardLevel';
+import {queryGroupMembersList} from "../../../redux/actions/saleCenterNEW/mySpecialActivities.action";
 
 // const FormItem = Form.Item;
 
 if (process.env.__CLIENT__ === true) {
     // require('../../../../client/componentsPage.less');
 }
-
+const FormItem = Form.Item;
+const Option = Select.Option;
 const Immutable = require('immutable');
 
 class StepTwo extends React.Component {
     constructor(props) {
         super(props);
+        const cardLevelRangeType = this.props.specialPromotion.getIn(['$eventInfo', 'cardLevelRangeType']);
         this.state = {
             message: '',
             settleUnitID: '',
             cardLevelIDList: [],
-            cardLevelRangeType: '0',
+            groupMembersID: this.props.specialPromotion.getIn(['$eventInfo', 'cardGroupID']),
+            groupMembersList: [],
+            cardLevelRangeType: cardLevelRangeType === undefined ? '5' : cardLevelRangeType,
         };
 
         this.handleSubmit = this.handleSubmit.bind(this);
+        this.handleSelectChange = this.handleSelectChange.bind(this);
         this.onCardLevelChange = this.onCardLevelChange.bind(this);
     }
 
@@ -50,6 +57,9 @@ class StepTwo extends React.Component {
             smsTemplate: smsGate === '1' || smsGate === '3' || smsGate === '4' ? this.state.message : '',
             cardLevelIDList: this.state.cardLevelIDList || [],
             cardLevelRangeType: this.state.cardLevelRangeType,
+        };
+        if (this.props.type == '51' && this.state.cardLevelRangeType == 5) {
+            opts.cardGroupID = this.state.groupMembersID;
         }
         if (smsGate === '1' || smsGate === '3' || smsGate === '4') {
             opts.settleUnitID = this.state.settleUnitID;
@@ -68,6 +78,13 @@ class StepTwo extends React.Component {
             cancel: undefined,
         });
         const specialPromotion = this.props.specialPromotion.get('$eventInfo').toJS();
+        const user = this.props.user;
+        const opts = {
+            _groupID: user.accountInfo.groupID, // 集团id
+            pageNo: 1,
+            pageSize: 1000,
+        };
+        this.props.queryGroupMembersList(opts);
         if (Object.keys(specialPromotion).length > 10) {
             this.setState({
                 message: specialPromotion.smsTemplate,
@@ -84,6 +101,35 @@ class StepTwo extends React.Component {
                 message: specialPromotion.smsTemplate,
             })
         }
+        if (this.props.type == '51' ) {
+            // 获取会员等级信息
+            if (nextProps.mySpecialActivities.$groupMembers) {
+                if (nextProps.mySpecialActivities.$groupMembers.groupMembersList instanceof Array && nextProps.mySpecialActivities.$groupMembers.groupMembersList.length > 0) {
+                    this.setState({
+                        groupMembersList: nextProps.mySpecialActivities.$groupMembers.groupMembersList,
+                    })
+                } else {
+                    this.setState({
+                        groupMembersList: [],
+                    })
+                }
+            }
+            if (this.props.specialPromotion.getIn(['$eventInfo', 'cardLevelRangeType']) !== nextProps.specialPromotion.getIn(['$eventInfo', 'cardLevelRangeType'])) {
+                const type = nextProps.specialPromotion.getIn(['$eventInfo', 'cardLevelRangeType']);
+                this.setState({cardLevelRangeType: type === undefined ? '5' : type});
+            }
+            if (this.props.specialPromotion.getIn(['$eventInfo', 'cardGroupID']) !== nextProps.specialPromotion.getIn(['$eventInfo', 'cardGroupID'])) {
+                this.setState({cardGroupID: nextProps.specialPromotion.getIn(['$eventInfo', 'cardGroupID'])});
+            }
+        }
+    }
+    // 会员群体Option
+    renderOptions() {
+        return  this.state.groupMembersList.map((groupMembers, index) => <Option key={groupMembers.groupMembersID}>{`${groupMembers.groupMembersName}【共${groupMembers.totalMembers}人】`}</Option>);
+
+    }
+    handleSelectChange(value) {
+        this.setState({groupMembersID: value});
     }
 
     render() {
@@ -91,12 +137,38 @@ class StepTwo extends React.Component {
         const sendFlag = info.smsGate === '1' || info.smsGate === '3' || info.smsGate === '4';
         return (
             <div>
-                <CardLevel
-                    onChange={this.onCardLevelChange}
-                    catOrCard="cat"
-                    type={this.props.type}
-                    form={this.props.form}
-                />
+                {this.state.cardLevelRangeType == '5' ?
+                    <FormItem
+                        label="会员群体"
+                        className={styles.FormItemStyle}
+                        labelCol={{ span: 4 }}
+                        wrapperCol={{ span: 17 }}
+                    >
+                        {this.props.form.getFieldDecorator('setgroupMembersID', {
+                            rules: [{
+                                required: true,
+                                message: '请选择会员群体',
+                            }],
+                            initialValue: this.state.groupMembersID,
+                        })(
+                            <Select
+                                style={{ width: '100%' }}
+                                placeholder="请选择会员群体"
+                                getPopupContainer={(node) => node.parentNode}
+                                onChange={this.handleSelectChange}
+                            >
+                                <Option key={'0'}>全部会员</Option>
+                                {this.renderOptions()}
+                            </Select>
+                        )
+                        }
+                    </FormItem>:
+                    <CardLevel
+                        onChange={this.onCardLevelChange}
+                        catOrCard="cat"
+                        type={this.props.type}
+                        form={this.props.form}
+                    />}
                 <SendMsgInfo
                     sendFlag={sendFlag}
                     form={this.props.form}
@@ -121,6 +193,8 @@ class StepTwo extends React.Component {
 const mapStateToProps = (state) => {
     return {
         specialPromotion: state.sale_specialPromotion_NEW,
+        user: state.user.toJS(),
+        mySpecialActivities: state.sale_mySpecialActivities_NEW.toJS(),
     };
 };
 
@@ -128,6 +202,9 @@ const mapDispatchToProps = (dispatch) => {
     return {
         setSpecialBasicInfo: (opts) => {
             dispatch(saleCenterSetSpecialBasicInfoAC(opts));
+        },
+        queryGroupMembersList: (opts) => {
+            dispatch(queryGroupMembersList(opts));
         },
     };
 };
