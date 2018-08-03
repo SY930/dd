16a1856a079a7b932e4e1fe8e8d608fc022ca83c
wuhao <wuhao@hualala.com>
2018-08-03 16:38:44 +0800

    排序接口迁移

--- src/containers/SaleCenterNEW/MyActivities/MyActivitiesShop.jsx
+++ src/containers/SaleCenterNEW/MyActivities/MyActivitiesShop.jsx
@@ -216,7 +216,7 @@ class MyActivitiesShop extends React.Component {
     }
     changeSortOrder(record, direction) {
         const params = {promotionID: record.promotionIDStr, shopID: this.props.user.shopID, rankingType: direction};
-        axiosData('/promotionV1/updatePromotionRanking.ajax', params, {needThrow: true}, {path: undefined}, 'HTTP_SERVICE_URL_PROMOTION_NEW').then(() => {
+        axiosData('/promotion/docPromotionService_updateRanking.ajax', params, {needThrow: true}, {path: undefined}, 'HTTP_SERVICE_URL_PROMOTION_NEW').then(() => {
             if (this.tableRef &&  this.tableRef.props && this.tableRef.props.pagination && this.tableRef.props.pagination.onChange) {
                 this.tableRef.props.pagination.onChange(this.tableRef.props.pagination.current, this.tableRef.props.pagination.pageSize);
             }
