d1e1166ef8a7ff3d109ce2d728b0a3bd654bab1f
wuhao <wuhao@hualala.com>
2018-08-14 17:37:10 +0800

    卸载新建短信模板的页面

--- src/containers/index.jsx
+++ src/containers/index.jsx
@@ -4,7 +4,7 @@ export { default as CrmDataValidator } from './CrmDataValidator/index';
 export { default as MySpecialActivities } from './SpecialPromotionNEW/MySpecialActivities/index';
 export { default as NewSpecialActivity } from './SpecialPromotionNEW/index';
 export { default as GiftInfo } from './GiftNew/GiftInfo/index';
-export { default as GiftAdd } from './GiftNew/GiftAdd/index';
+// export { default as GiftAdd } from './GiftNew/GiftAdd/index';
 
 /*export { default as old_MyActivities } from './SaleCenter/MyActivities/MyActivities';
 export { default as old_NewActivity } from './SaleCenter/index';
