f99f12e766c3d4c91229f0313f21fac1a6703be9
wuhao <wuhao@hualala.com>
2018-09-18 17:20:16 +0800

    调整微信模板默认文案

--- src/constants/weChatTemplateConstants.jsx
+++ src/constants/weChatTemplateConstants.jsx
@@ -1,12 +1,12 @@
 export const DEFAULT_WECHAT_TEMPLATE_CONFIG = {
     '1': {// 礼品到期提醒
         title: '亲，您有电子券即将到期，不要让白花花的银子溜走哦',
-        remark: '请您尽快到店使用',
+        remark: '望您尽快到店使用',
         type: '会员到期提醒'
     },
     '2': {// 礼品领取成功通知
-        title: '送礼啦！',
-        remark: '感谢您的支持',
+        title: '送礼啦',
+        remark: '感谢你的支持',
         type: '礼品领取成功通知'
     }
 };
