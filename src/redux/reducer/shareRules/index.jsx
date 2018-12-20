52d8f61358a2f5da7a9878020ab97900768e7a81
吴豪 <wuhao@hualala.com>
2018-12-20 09:49:17 +0800

    temp ci

--- src/redux/reducer/shareRules/index.jsx
+++ src/redux/reducer/shareRules/index.jsx
@@ -26,7 +26,6 @@ const $initialState = Immutable.fromJS({
     /* 查回来的已设置的共享组 */
     shareGroups: [],
     isQuerying: false,
-    hasQueryError: false,
     /* 可供选择的所有活动列表 */
     availablePromotionList: [],
     searchPromotionType: '',
@@ -34,12 +33,59 @@ const $initialState = Immutable.fromJS({
     isCreate: false,
     isEdit: false,
     isSaving: false,
+    isDeleting: false,
+    isRemoving: false,
 
 });
 
 export const share_rules = ($$state = $initialState, action) => {
     switch (action.type) {
+        case SALE_CENTER_QUERY_SHARE_GROUP_START:
+            return $$state.set('isQuerying', true);
+        case SALE_CENTER_QUERY_SHARE_GROUP_SUCCESS:
+            return $$state
+                .set('isQuerying', false)
+                .set('shareGroups', Immutable.fromJS(action.payload))
+        case SALE_CENTER_QUERY_SHARE_GROUP_FAIL:
+            return $$state
+                .set('isQuerying', false)
 
+        case SALE_CENTER_CHANGE_QUERY_PROMOTION_TYPE:
+            return $$state
+                .set('searchPromotionType', action.payload)
+        case SALE_CENTER_CHANGE_QUERY_PROMOTION_NAME:
+            return $$state
+                .set('searchPromotionName', action.payload)
+
+        case SALE_CENTER_SAVE_SHARE_GROUP_START:
+            return $$state
+                .set('isSaving', true)
+        case SALE_CENTER_SAVE_SHARE_GROUP_SUCCESS:
+            return $$state
+                .set('isSaving', false)
+        case SALE_CENTER_SAVE_SHARE_GROUP_FAIL:
+            return $$state
+                .set('isSaving', false)
+
+        case SALE_CENTER_DELETE_SHARE_GROUP_START:
+            return $$state
+                .set('isDeleting', true)
+        case SALE_CENTER_DELETE_SHARE_GROUP_SUCCESS:
+            return $$state
+                .set('isDeleting', false)
+        case SALE_CENTER_DELETE_SHARE_GROUP_FAIL:
+            return $$state
+                .set('isDeleting', false)
+
+        case SALE_CENTER_REMOVE_ITEM_FROM_SHARE_GROUP_START:
+            return $$state
+                .set('isRemoving', true)
+        case SALE_CENTER_REMOVE_ITEM_FROM_SHARE_GROUP_SUCCESS:
+            return $$state
+                .set('isRemoving', false)
+        case SALE_CENTER_REMOVE_ITEM_FROM_SHARE_GROUP_FAIL:
+            return $$state
+                .set('isRemoving', false)
         default: return $$state;
     }
 };
