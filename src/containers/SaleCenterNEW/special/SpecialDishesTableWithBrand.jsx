cc9d90178f3190928b651b5bec0e47de52feccd6
wuhao <wuhao@hualala.com>
2019-04-04 17:26:43 +0800

    特价菜新组件

---
+++ src/containers/SaleCenterNEW/special/SpecialDishesTableWithBrand.jsx
@@ -0,0 +1,41 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import {
+    Row,
+    Col,
+    Table,
+    Input,
+    message,
+    Modal,
+    Form,
+} from 'antd';
+
+const mapStateToProps = (state) => {
+    return {
+
+    }
+}
+
+const mapDispatchToProps = (dispatch) => {
+    return {
+
+    }
+}
+
+class SpecialDishesTableWithBrand extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            selectorModalVisible: false,
+            
+        }
+    }
+    render() {
+        return (
+            <div>
+
+            </div>
+        )
+    }
+}
