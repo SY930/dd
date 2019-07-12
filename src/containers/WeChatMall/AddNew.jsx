57499752021319de6c53242709e6716cdf7002c8
wuhao <wuhao@hualala.com>
2019-07-12 16:11:41 +0800

    Revert "屏蔽秒杀活动"
    This reverts commit 8846ffad234df31555b003336bc2fcc104aff580.

--- src/containers/WeChatMall/AddNew.jsx
+++ src/containers/WeChatMall/AddNew.jsx
@@ -154,7 +154,6 @@ class NewActivity extends React.Component {
                         key={activity.key}
                         style={{
                             listStyle: 'none',
-                            display: activity.key == 72 ? 'none' : 'inherit'
                         }}
                     >
                         <Authority rightCode={BASIC_PROMOTION_CREATE}>
