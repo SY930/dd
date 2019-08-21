f9351fc015a55e19264db8f24a054b4c9c621409
wuhao <wuhao@hualala.com>
2019-08-21 16:46:27 +0800

    temp ci

--- src/containers/PromotionCalendarNew/EntryPage.jsx
+++ src/containers/PromotionCalendarNew/EntryPage.jsx
@@ -4,15 +4,25 @@ import {
     DatePicker,
     Select,
     Icon,
+    TreeSelect,
 } from 'antd';
+import moment from 'moment';
+import Immutable from 'immutable';
 import { jumpPage } from '@hualala/platform-base';
+import { connect } from 'react-redux';
 import { axiosData } from '../../helpers/util'
 import registerPage from '../../../index';
 import { PROMOTION_CALENDAR_NEW, SALE_CENTER_PAGE } from '../../constants/entryCodes';
 import style from './style.less';
 import PromotionCreateModal from '../NewCreatePromotions/PromotionCreateModal'
 import CalendarList from './CalendarList';
-import moment from 'moment';
+import {
+    fetchPromotionScopeInfo,
+} from '../../redux/actions/saleCenterNEW/promotionScopeInfo.action';
+import {
+
+} from '../../constants/promotionType';
+
 
 const Option = Select.Option;
 const { MonthPicker } = DatePicker;
@@ -54,18 +64,80 @@ const CHANNEL_LIST = [
     },
 ];
 
+const mapDispatchToProps = (dispatch) => {
+    return {
+        // 查询品牌、店铺等信息
+        fetchPromotionScopeInfo: (opts) => {
+            dispatch(fetchPromotionScopeInfo(opts));
+        },
+    };
+};
+const mapStateToProps = (state) => {
+    return {
+        promotionScopeInfo: state.sale_promotionScopeInfo_NEW,
+        user: state.user,
+    };
+};
+
 @registerPage([PROMOTION_CALENDAR_NEW])
+@connect(mapStateToProps, mapDispatchToProps)
 export default class EntryPage extends Component {
 
     constructor() {
         super();
         this.state = {
             createModalVisible: false,
+            loading: false,
+            selectedShops: [],
             shopIDList: [],
             channelList: [],
             supportOrderTypeList: [],
             startDate: moment(),
+            promotionInfoList: [
+                {"eventID":'6665094687050701717',"eventName":"0306完善资料送礼","eventCategory":20,"eventType":60,"eventStartDate":20000101,"eventEndDate":29991231},
+                {"eventID": '950957556455571456',"eventName":"引擎满减","eventCategory":10,"eventType":2010,"eventStartDate":20000101,"eventEndDate":29991231},
+            ],
         }
+    }
+
+    handleQuery = () => {
+        const {
+            shopIDList,
+            channelList,
+            supportOrderTypeList,
+            startDate,
+        } = this.state;
+        this.setState({
+            loading: true,
+        })
+        axiosData(
+            '/calendar/eventCalendarService_queryPromotionList.ajax',
+            {
+                shopIDList,
+                channelList,
+                supportOrderTypeList,
+                startDate: startDate.format('YYYYMM01')
+            },
+            {},
+            { path: '' },
+            'HTTP_SERVICE_URL_PROMOTION_NEW'
+        ).then(data => {
+            console.log(data)
+            this.setState({
+                loading: false,
+            })
+        }).catch(err => {
+            this.setState({
+                loading: false,
+            })
+        })
+    }
+
+    componentDidMount() {
+        this.props.fetchPromotionScopeInfo({
+            _groupID: this.props.user.getIn(['accountInfo','groupID'])
+        });
+        this.handleQuery();
     }
 
     openCreateModal = () => {
@@ -87,25 +159,72 @@ export default class EntryPage extends Component {
         })
     }
 
+    onTreeSelect(value, treeData) {        
+        const shopsInfo = [];
+        treeData.forEach((td) => {
+            if (td.children) {
+                td.children.map((tdc) => {
+                    shopsInfo.push(tdc);
+                })
+            }
+        });
+        const shopIDList = value.map(val => shopsInfo.find((si) => {
+                    return si.value === val;
+                }).shopID)
+        this.setState({
+            selectedShops: value,
+            shopIDList,
+        })
+    }
+
+    renderShopsInTreeSelectMode() {
+        const treeData = Immutable.List.isList(this.props.promotionScopeInfo.getIn(['refs', 'data', 'constructedData'])) ?
+            this.props.promotionScopeInfo.getIn(['refs', 'data', 'constructedData']).toJS() :
+            this.props.promotionScopeInfo.getIn(['refs', 'data', 'constructedData']);
+        const tProps = {
+                treeData,
+                value: this.state.selectedShops,
+                onChange: value => this.onTreeSelect(value, treeData),
+                placeholder: '请选择店铺',
+                allowClear: true,
+            };
+        return (
+            <TreeSelect
+                showSearch
+                multiple
+                {...tProps}
+                style={{ width: 160, margin: '0 20px 0 10px' }}
+                dropdownStyle={{ minWidth: 160 }}
+                dropdownMatchSelectWidth={false}
+                treeNodeFilterProp="label"
+            />
+        );
+    }
+
     renderFilterBar() {
         const {
             shopIDList,
             channelList,
             supportOrderTypeList,
             startDate,
+            loading,
         } = this.state;
         return (
             <div className={style.searchHeader}>
                 <h5>月份</h5>
                 <MonthPicker
+                    allowClear={false}
                     style={{ width: 160, margin: '0 20px' }}
+                    format="YYYY年MM月"
                     value={startDate}
                     onChange={this.handleMonthChange}
                 />
+                <h5>适用店铺</h5>
+                {this.renderShopsInTreeSelectMode()}
                 <h5>适用场景</h5>
                 <Select
                     multiple={true}
-                    style={{ width: 160, margin: '0 20px' }}
+                    style={{ width: 200, margin: '0 20px 0 10px' }}
                     value={channelList}
                     placeholder="请选择场景"
                     onChange={this.handleChannelListChange}
@@ -120,7 +239,7 @@ export default class EntryPage extends Component {
                 <Select
                     multiple={true}
                     value={supportOrderTypeList}
-                    style={{ width: 160, margin: '0 20px' }}
+                    style={{ width: 200, margin: '0 20px 0 10px' }}
                     placeholder="请选择业务"
                     onChange={this.handleSupportOrderTypeChange}
                 >
@@ -130,8 +249,14 @@ export default class EntryPage extends Component {
                         ))
                     }
                 </Select>
-        
-                <Button type="primary"><Icon type="search" />查询</Button>
+                <Button
+                    type="primary"
+                    loading={loading}
+                    onClick={this.handleQuery}
+                >
+                    <Icon type="search" />
+                    查询
+                </Button>
             </div>
         );
     }
