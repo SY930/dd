f194aa5836a1c2c79efcf734eb968de53b30259a
wuhao <wuhao@hualala.com>
2019-08-21 10:25:02 +0800

    temp ci

--- src/containers/PromotionCalendarNew/CalendarList.jsx
+++ src/containers/PromotionCalendarNew/CalendarList.jsx
@@ -2,13 +2,65 @@ import React, { Component } from 'react';
 import style from './style.less';
 import moment from 'moment';
 
-export default class CalendarList extends Component {
+const MONTH_FORMAT = 'YYYYMM';
+const DAY_FORMAT = 'YYYYMMDD';
+const DAY_LENGTH_IN_PX = 20;
+class CalendarList extends Component {
+    constructor() {
+        super();
+        this.state = {
+            x: 0,
+            y: 0,
+            /** 固定显示3个月的区间 */
+            monthsCount: 3,
+        }
+    }
 
     getMonthStr = () => {
         return `${moment().format('M')}月`
     }
 
+    getIntervalInfo = () => {
+        const { startMonth } = this.props;
+        const { monthsCount } = this.state;
+        const monthsInfo = [];
+        let totalDaysCount = 0;
+        for (let i = 0; i < monthsCount; i ++) {
+            const monthMoment = moment(startMonth, MONTH_FORMAT).add(i, 'M');
+            const monthDisplayStr = monthMoment.format('YYYY年MM月');
+            const daysCount = monthMoment.daysInMonth();
+            totalDaysCount += daysCount;
+            monthsInfo.push({
+                monthMoment,
+                monthDisplayStr,
+                daysCount,
+            })
+        }
+        return {
+            monthsInfo,
+            totalDaysCount,
+        };
+    }
+
+
+    handleCanlendarScroll = (e) => {
+        const {
+            scrollTop,
+            scrollLeft,
+        } = e.target;
+        this.setState({
+            x: scrollLeft,
+            y: scrollTop,
+        })
+    }
+
     render() {
+        const { x, y } = this.state;
+        const {
+            monthsInfo,
+            totalDaysCount,
+        } = this.getIntervalInfo();
+        const totalIntervalLength = DAY_LENGTH_IN_PX * (totalDaysCount + monthsInfo.length);
         return (
             <div className={style.calendarListWrapper}>
                 <div className={style.buttonArea}>
@@ -16,11 +68,70 @@ export default class CalendarList extends Component {
                         {this.getMonthStr()}
                     </div>
                 </div>
+                <div className={style.menuWrapper}>
+                    <div style={{ transform: `translateY(${-y}px)`}} className={style.menu}>
+                    
+                    </div>
+                </div>
 
-                <div className={style.calendarMain}>
-
+                <div onScroll={this.handleCanlendarScroll} className={style.calendarMain}>
+                    <div
+                        className={style.canvas}
+                        style={{
+                            width: totalIntervalLength,
+                        }}
+                    >
+                    </div>
+                </div>
+                <div className={style.timelineWrapper}>
+                    <div
+                        style={{
+                            transform: `translateX(${-x}px)`,
+                            width: totalIntervalLength,
+                        }}
+                        className={style.timeline}
+                    >
+                        {
+                            monthsInfo.map(monthInfo => (
+                                <div
+                                    className={style.monthBlock}
+                                    style={{ width: (monthInfo.daysCount + 1) * DAY_LENGTH_IN_PX }}
+                                >
+                                    {monthInfo.monthDisplayStr}
+                                    {
+                                        new Array(monthInfo.daysCount).fill(0).map((_, index, arr) => {
+                                            const isToday = moment().format(DAY_FORMAT) ===
+                                                monthInfo.monthMoment.set('date', index + 1).format(DAY_FORMAT);
+                                            return (
+                                                <div
+                                                    key={`${index}`}
+                                                    className={style.dayMark}
+                                                    style={{
+                                                        width: (index + 1) % 5 === 0 ? 10 : 6,
+                                                        height: (index + 1) % 5 === 0 ? 10 : 6,
+                                                        left: (index + 1) * DAY_LENGTH_IN_PX,
+                                                        background: isToday ? '#379FF1' : '#C2C2C2'
+                                                    }}
+                                                >
+                                                    {isToday && <div className={style.todayTag}>{index + 1}</div>}
+                                                    {isToday && <div className={style.todayLine}></div>}
+                                                    {index === 0 && <div className={style.startOfMonthLine}></div>}
+                                                </div>
+                                            )
+                                        })
+                                    }
+                                </div>
+                            ))
+                        }
+                    </div>
                 </div>
             </div>
         )
     }
 }
+
+CalendarList.defaultProps = {
+    startMonth: '201908',
+};
+
+export default CalendarList;
--- src/containers/PromotionCalendarNew/EntryPage.jsx
+++ src/containers/PromotionCalendarNew/EntryPage.jsx
@@ -12,9 +12,47 @@ import { PROMOTION_CALENDAR_NEW, SALE_CENTER_PAGE } from '../../constants/entryC
 import style from './style.less';
 import PromotionCreateModal from '../NewCreatePromotions/PromotionCreateModal'
 import CalendarList from './CalendarList';
+import moment from 'moment';
 
 const Option = Select.Option;
 const { MonthPicker } = DatePicker;
+
+const SUPPORT_ORDER_TYPES = [
+    {
+        label: '预定',
+        value: '10',
+    },
+    {
+        label: '闪吃',
+        value: '11',
+    },
+    {
+        label: '外送',
+        value: '20',
+    },
+    {
+        label: '堂食',
+        value: '31',
+    },
+    {
+        label: '自提',
+        value: '21',
+    },
+];
+const CHANNEL_LIST = [
+    {
+        value: 'POS',
+        label: '云店',
+    },
+    {
+        value: 'WECHAT',
+        label: '微信',
+    },
+        {
+        value: 'YST',
+        label: '饮食通',
+    },
+];
 
 @registerPage([PROMOTION_CALENDAR_NEW])
 export default class EntryPage extends Component {
@@ -23,27 +61,74 @@ export default class EntryPage extends Component {
         super();
         this.state = {
             createModalVisible: false,
+            shopIDList: [],
+            channelList: [],
+            supportOrderTypeList: [],
+            startDate: moment(),
         }
     }
 
     openCreateModal = () => {
         this.setState({createModalVisible: true})
     }
+    handleMonthChange = (v) => {
+        this.setState({
+            startDate: v,
+        })
+    }
+    handleChannelListChange = (v) => {
+        this.setState({
+            channelList: v,
+        })
+    }
+    handleSupportOrderTypeChange = (v) => {
+        this.setState({
+            supportOrderTypeList: v,
+        })
+    }
 
     renderFilterBar() {
+        const {
+            shopIDList,
+            channelList,
+            supportOrderTypeList,
+            startDate,
+        } = this.state;
         return (
             <div className={style.searchHeader}>
-                <h5>活动时间</h5>
-                <MonthPicker style={{ width: '160px' }} placeholder="Select month" />
-                <h5>使用状态</h5>
+                <h5>月份</h5>
+                <MonthPicker
+                    style={{ width: 160, margin: '0 20px' }}
+                    value={startDate}
+                    onChange={this.handleMonthChange}
+                />
+                <h5>适用场景</h5>
                 <Select
-                    style={{ width: '160px' }}
-                    defaultValue="0"
-                    placeholder="请选择使用状态"
-                    onChange={(value) => {
-                    }}
+                    multiple={true}
+                    style={{ width: 160, margin: '0 20px' }}
+                    value={channelList}
+                    placeholder="请选择场景"
+                    onChange={this.handleChannelListChange}
                 >
-                    <Option value='1' >全部</Option>
+                    {
+                        CHANNEL_LIST.map(({value, label}) => (
+                            <Option key={value}>{label}</Option>
+                        ))
+                    }
+                </Select>
+                <h5>适用业务</h5>
+                <Select
+                    multiple={true}
+                    value={supportOrderTypeList}
+                    style={{ width: 160, margin: '0 20px' }}
+                    placeholder="请选择业务"
+                    onChange={this.handleSupportOrderTypeChange}
+                >
+                    {
+                        SUPPORT_ORDER_TYPES.map(({value, label}) => (
+                            <Option key={value}>{label}</Option>
+                        ))
+                    }
                 </Select>
         
                 <Button type="primary"><Icon type="search" />查询</Button>
