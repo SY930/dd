ad3194dea2ef04a9a940a6c078df11e09a384986
wuhao <wuhao@hualala.com>
2019-10-14 15:03:36 +0800

    膨胀大礼包简易版完工

--- src/containers/PromotionDecoration/ExpasionGiftDecorationBoard.jsx
+++ src/containers/PromotionDecoration/ExpasionGiftDecorationBoard.jsx
@@ -5,10 +5,16 @@ import ColorSettingBlock from './ColorSettingBlock'
 import DecorationUploader from './DecorationUploader';
 import phoneImg from './assets/iphone.png';
 import defaultExpansionBgImg from './assets/expansionBg.png'
-import defaultEndImg from './assets/recommend2.png'
+import giftExample from './assets/gift-example.png'
+import giftExampleThumb from './assets/giftExampleThumb.png'
 import phoneTop from './assets/phoneTop.png'
 import ButtonSettingBlock from './ButtonSettingBlock'
 import WrappedColorPicker from '../../components/common/WrappedColorPicker';
+import giftBg1 from './assets/1-1.png'
+import giftBg2 from './assets/1-2.png'
+import progressImg from './assets/progress.png'
+import giftBg3 from './assets/2-1.png'
+import giftBg4 from './assets/2-2.png'
 
 const { TabPane } = Tabs;
 
@@ -19,7 +25,6 @@ export default class ExpasionGiftDecorationBoard extends Component {
     }
 
     renderPhonePreview() {
-        const { tabKey } = this.state;
         const {
             decorationInfo: {
                 bannerImg = defaultExpansionBgImg,
@@ -47,8 +52,186 @@ export default class ExpasionGiftDecorationBoard extends Component {
                         活动还剩：2天17小时10分30秒
                     </div>
                     <div className={style.giftContainer}>
-                        <div className={style.buttonArea}>
-
+                        <div className={style.giftWrapper}>
+                            <img
+                                style={{
+                                    width: '100%',
+                                    position: 'absolute',
+                                    top: 0,
+                                }}
+                                src={giftBg1}
+                                alt=""
+                            />
+                            <img
+                                src={giftImg1 || giftExample}
+                                alt=""
+                                style={{
+                                    width: 186,
+                                    height: 59,
+                                    position: 'absolute',
+                                    top: 7,
+                                    left: 13,
+                                }}
+                            />
+                            <img
+                                style={{
+                                    width: '100%',
+                                    position: 'absolute',
+                                    bottom: 0,
+                                }}
+                                src={giftBg2}
+                                alt=""
+                            />
+                        </div>
+                        <div
+                            className={style.buttonArea}
+                            style={{
+                                color: buttonColor,
+                                background: buttonBgColor,
+                            }}
+                        >
+                            立即参与
+                        </div>
+                    </div>
+                    <div className={style.detailBlock}>
+                        <div style={{ background: tagColor1 }} className={style.coloredTag}>
+                            活动好礼
+                        </div>
+                        <img className={style.progressBar} src={progressImg} alt=""/>
+                        <div className={style.giftRow}>
+                            <div className={style.subWrapper}>
+                                <div className={style.giftItem}>
+                                <img
+                                    style={{
+                                        width: '100%',
+                                        position: 'absolute',
+                                        top: 0,
+                                    }}
+                                    src={giftBg3}
+                                    alt=""
+                                />
+                                <img
+                                    src={giftThumbImg1 || giftExampleThumb}
+                                    alt=""
+                                    style={{
+                                        width: 51,
+                                        height: 59,
+                                        position: 'absolute',
+                                        top: 8.5,
+                                        left: 7,
+                                    }}
+                                />
+                                <img
+                                    style={{
+                                        width: '100%',
+                                        position: 'absolute',
+                                        bottom: 0,
+                                    }}
+                                    src={giftBg4}
+                                    alt=""
+                                />
+                                </div>
+                                <div className={style.action}>
+                                    参与即领
+                                </div>
+                            </div>
+                            <div className={style.subWrapper}>
+                                <div className={style.giftItem}>
+                                <img
+                                    style={{
+                                        width: '100%',
+                                        position: 'absolute',
+                                        top: 0,
+                                    }}
+                                    src={giftBg3}
+                                    alt=""
+                                />
+                                <img
+                                    src={giftThumbImg2 || giftExampleThumb}
+                                    alt=""
+                                    style={{
+                                        width: 51,
+                                        height: 59,
+                                        position: 'absolute',
+                                        top: 8.5,
+                                        left: 7,
+                                    }}
+                                />
+                                <img
+                                    style={{
+                                        width: '100%',
+                                        position: 'absolute',
+                                        bottom: 0,
+                                    }}
+                                    src={giftBg4}
+                                    alt=""
+                                />
+                                </div>
+                                <div className={style.action}>
+                                    邀3人领取
+                                </div>
+                            </div>
+                            <div className={style.subWrapper}>
+                                <div className={style.giftItem}>
+                                <img
+                                    style={{
+                                        width: '100%',
+                                        position: 'absolute',
+                                        top: 0,
+                                    }}
+                                    src={giftBg3}
+                                    alt=""
+                                />
+                                <img
+                                    src={giftThumbImg3 || giftExampleThumb}
+                                    alt=""
+                                    style={{
+                                        width: 51,
+                                        height: 59,
+                                        position: 'absolute',
+                                        top: 8.5,
+                                        left: 7,
+                                    }}
+                                />
+                                <img
+                                    style={{
+                                        width: '100%',
+                                        position: 'absolute',
+                                        bottom: 0,
+                                    }}
+                                    src={giftBg4}
+                                    alt=""
+                                />
+                                </div>
+                                <div className={style.action}>
+                                    邀5人领取
+                                </div>
+                            </div>
+                                    
+                        </div>
+                        <div className={style.detailTxt}>
+                            免费领取礼包，更多美味更多实惠等你来！邀请好友，更能升级礼包，更多惊喜等你哦！
+                        </div>
+                    </div>
+                    <div className={style.friendsInfo}>
+                        <div style={{ background: tagColor2 }} className={style.coloredTag}>
+                            助力好友榜
+                        </div>
+                        <div className={style.tip}>
+                            暂无好友助力，快去邀请好友助你升级礼包吧
+                        </div>
+                    </div>
+                    <div className={style.ruleInfo}>
+                        <div style={{ background: tagColor3 }} className={style.coloredTag}>
+                            活动规则
+                        </div>
+                        <div className={style.rule}>
+                            <p>发起人规则：</p>
+                            <p>1、同一位用户在一个活动中，只能发起一次膨胀大礼包活动</p>
+                            <p>2、兑换奖品后活动立即终止</p>
+                            <p style={{ marginTop: 10 }}>助力规则：</p>
+                            <p>1、同一位助力用户只能为同一位发起者膨胀一次，不可多次膨胀</p>
+                            <p>2、同一位助力用户可为不同发起者各膨胀一次</p>
                         </div>
                     </div>
                 </div>
@@ -74,7 +257,7 @@ export default class ExpasionGiftDecorationBoard extends Component {
                         />
                         <div className={style.uploaderTip}>
                             <p>* 图片大小不要超过1000KB</p>
-                            <p>* 建议尺寸920x360像素</p>
+                            <p>* 建议尺寸<span style={{ color: '#379FF1' }}>750*666</span>像素</p>
                             <p>* 支持JPG、PNG、GIF图片文件</p>
                         </div>
                     </div>
@@ -88,7 +271,7 @@ export default class ExpasionGiftDecorationBoard extends Component {
                         />
                         <div className={style.uploaderTip}>
                             <p>* 图片大小不要超过1000KB</p>
-                            <p>* 建议尺寸920x360像素</p>
+                            <p>* 建议尺寸<span style={{ color: '#379FF1' }}>134*146</span>像素</p>
                             <p>* 支持JPG、PNG、GIF图片文件</p>
                         </div>
                     </div>
@@ -102,7 +285,7 @@ export default class ExpasionGiftDecorationBoard extends Component {
                         />
                         <div className={style.uploaderTip}>
                             <p>* 图片大小不要超过1000KB</p>
-                            <p>* 建议尺寸920x360像素</p>
+                            <p>* 建议尺寸<span style={{ color: '#379FF1' }}>485*146</span>像素</p>
                             <p>* 支持JPG、PNG、GIF图片文件</p>
                         </div>
                     </div>
@@ -116,7 +299,7 @@ export default class ExpasionGiftDecorationBoard extends Component {
                         />
                         <div className={style.uploaderTip}>
                             <p>* 图片大小不要超过1000KB</p>
-                            <p>* 建议尺寸920x360像素</p>
+                            <p>* 建议尺寸<span style={{ color: '#379FF1' }}>134*146</span>像素</p>
                             <p>* 支持JPG、PNG、GIF图片文件</p>
                         </div>
                     </div>
@@ -130,7 +313,7 @@ export default class ExpasionGiftDecorationBoard extends Component {
                         />
                         <div className={style.uploaderTip}>
                             <p>* 图片大小不要超过1000KB</p>
-                            <p>* 建议尺寸920x360像素</p>
+                            <p>* 建议尺寸<span style={{ color: '#379FF1' }}>485*146</span>像素</p>
                             <p>* 支持JPG、PNG、GIF图片文件</p>
                         </div>
                     </div>
@@ -144,7 +327,7 @@ export default class ExpasionGiftDecorationBoard extends Component {
                         />
                         <div className={style.uploaderTip}>
                             <p>* 图片大小不要超过1000KB</p>
-                            <p>* 建议尺寸920x360像素</p>
+                            <p>* 建议尺寸<span style={{ color: '#379FF1' }}>134*146</span>像素</p>
                             <p>* 支持JPG、PNG、GIF图片文件</p>
                         </div>
                     </div>
@@ -158,7 +341,7 @@ export default class ExpasionGiftDecorationBoard extends Component {
                         />
                         <div className={style.uploaderTip}>
                             <p>* 图片大小不要超过1000KB</p>
-                            <p>* 建议尺寸920x360像素</p>
+                            <p>* 建议尺寸<span style={{ color: '#379FF1' }}>485*146</span>像素</p>
                             <p>* 支持JPG、PNG、GIF图片文件</p>
                         </div>
                     </div>
@@ -201,7 +384,7 @@ export default class ExpasionGiftDecorationBoard extends Component {
                         />
                     </div>
                 </div>
-                <div style={{ margin: 0, paddingBottom: 20, borderBottom: '1px solid #EEE' }} className={style.sectionWrapper}>
+                <div style={{ margin: 0, paddingBottom: 20 }} className={style.sectionWrapper}>
                     <div style={{color: '#333', fontSize: 14, margin: '20px 0 10px 0'}}>标签颜色</div>
                     <div className={style.tagColorBlock}>
                         <div className={style.leftBlock}>
@@ -258,12 +441,6 @@ export default class ExpasionGiftDecorationBoard extends Component {
         )
     }
     render() {
-        const {
-            decorationInfo: {
-                enterImg,
-            },
-            onChange,
-        } = this.props;
         return (
             <div className={style.boardWrapper}>
                 {this.renderPhonePreview()}
