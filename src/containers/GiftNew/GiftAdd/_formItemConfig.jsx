fdfbd68a3f9b22be70b73847c52f0b5f19a55731
wuhao <wuhao@hualala.com>
2019-10-18 16:18:51 +0800

    单天使用张数限制

--- src/containers/GiftNew/GiftAdd/_formItemConfig.jsx
+++ src/containers/GiftNew/GiftAdd/_formItemConfig.jsx
@@ -132,6 +132,21 @@ const FORMITEMS = {
             message: '张数范围1~999999'
         }],
     },
+    customerUseCountLimit: {
+        label: '会员单天使用张数限制',
+        type: 'text',
+        placeholder: '不填表示不限制',
+        surfix: '张',
+        rules: [{
+            validator: (rule, v = '', cb) => {
+                if (/^(?:[1-9][0-9]{0,5})?$/.test(v)) {
+                    return cb()
+                }
+                cb(rule.message);
+            },
+            message: '张数范围1~999999'
+        }],
+    },
     isFoodCatNameList: {
         // label: '抵扣规则',
         // type: 'combo',
@@ -464,6 +479,7 @@ const SECOND_KEYS = (() => ({
                 'isOfflineCanUsing',
                 'giftShareType',
                 'moneyLimitType',
+                'customerUseCountLimit',
                 'shopNames',
                 'amountType',
                 'showGiftRule',
