7d01d11dd691b1e4f94a506e50427c36235bd19d
wuhao <wuhao@hualala.com>
2019-12-03 18:57:34 +0800

    npe

--- src/containers/SpecialPromotionNEW/birthdayGift/stepTwo.jsx
+++ src/containers/SpecialPromotionNEW/birthdayGift/stepTwo.jsx
@@ -121,7 +121,7 @@ class StepTwo extends React.Component {
             if (this.props.specialPromotion.getIn(['$eventInfo', 'excludeCardTypeShops'])
                 !== nextProps.specialPromotion.getIn(['$eventInfo', 'excludeCardTypeShops'])) {
                 const occupiedShops = nextProps.specialPromotion.getIn(['$eventInfo', 'excludeCardTypeShops']).toJS().reduce((acc, curr) => {
-                    acc.push(...curr.shopIDList.map(id => `${id}`)); // 把shopID转成string, 因为基本档返回的是string
+                    acc.push(...(curr.shopIDList || []).map(id => `${id}`)); // 把shopID转成string, 因为基本档返回的是string
                     return acc;
                 }, []);
                 this.setState({ 
