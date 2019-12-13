93570160702f97159a69da633b01355ad7b39ffd
wuhao <wuhao@hualala.com>
2019-12-13 14:15:43 +0800

    调整分页取值

--- src/containers/GiftNew/components/GenerateBatchGifts.jsx
+++ src/containers/GiftNew/components/GenerateBatchGifts.jsx
@@ -82,12 +82,12 @@ class GenerateBatchGifts extends Component {
             params.startDate = this.state.queryDateRange[0].format('YYYYMMDD');
             params.endDate = this.state.queryDateRange[1].format('YYYYMMDD');
         }
-        axiosData('/gift/getCouponBatch.ajax', {...params, pageNo}, {}, {path: 'giftBatchResList'}, 'HTTP_SERVICE_URL_PROMOTION_NEW')
+        axiosData('/gift/getCouponBatch.ajax', {...params, pageNo}, {}, {path: ''}, 'HTTP_SERVICE_URL_PROMOTION_NEW')
             .then(res => {
                 this.setState({
-                    historyList: res || [],
-                    total: (res || []).length ? (res.totalSize || 0) : 0,
-                    pageNo: (res || []).length ? (res.pageNo || 1) : 1,
+                    historyList: res.giftBatchResList || [],
+                    total: (res.giftBatchResList || []).length ? (res.totalSize || 0) : 0,
+                    pageNo: (res.giftBatchResList || []).length ? (res.pageNo || 1) : 1,
                     loading: false,
                 });
             })
