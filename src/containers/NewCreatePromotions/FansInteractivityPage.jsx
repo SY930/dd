1d254e4fb3bfaa4231fa647f3255188dcf5eb6ea
dangxiaorun <dangxiaorun@hualala.com>
2020-01-09 10:09:44 +0800

    [chg]

--- src/containers/NewCreatePromotions/FansInteractivityPage.jsx
+++ src/containers/NewCreatePromotions/FansInteractivityPage.jsx
@@ -4,18 +4,52 @@ import BasePage from "./BasePage";
 import registerPage from '../../../index';
 import {FANS_INTERACTIVITY_PROMOTION_TYPES} from "../../constants/promotionType";
 import {FANS_INTERACTIVITY} from "../../constants/entryCodes";
+import { axiosData } from '../../helpers/util';
 
+const limitedTypes = [
+    '77',
+]
 @registerPage([FANS_INTERACTIVITY], {
 })
 @connect(mapStateToProps, mapDispatchToProps)
+
 class NewCustomerPage extends Component {
+
+    state = {
+        whiteList: [],
+    }
+
+    componentDidMount() {
+        axiosData(
+            '/promotionWhiteList/queryAllData.ajax',
+            { requestType: '10', },
+            { needThrow: true },
+            { path: 'allData' },
+            'HTTP_SERVICE_URL_PROMOTION_NEW',
+        ).then(dataStr => {
+            const dataArr = JSON.parse(dataStr);
+            const { whiteList } = this.state;
+            dataArr.forEach(item => {
+                if (item.isWhiteList) {
+                    whiteList.push(`${item.specialType}`)
+                }
+            })
+            this.setState({ whiteList });
+        })
+    }
+
+    promotionFilter = (promotionType) => {
+        const index = limitedTypes.indexOf(promotionType);
+        if (index === -1) return true;
+        return this.state.whiteList.includes(promotionType);
+    }
 
     render() {
         return (
             <BasePage
                 categoryTitle="粉丝互动"
                 promotions={
-                    FANS_INTERACTIVITY_PROMOTION_TYPES
+                    FANS_INTERACTIVITY_PROMOTION_TYPES.filter(item => this.promotionFilter(item.key))
                 }
             />
         )
