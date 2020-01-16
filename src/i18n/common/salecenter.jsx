48d9728e27e6dde7aad8c6d1c46a7a5e64313273
龚法曾 <gongfazeng@hualala.com>
2020-01-16 11:20:55 +0800

    国际化 满赠第三步 弄完

--- src/i18n/common/salecenter.jsx
+++ src/i18n/common/salecenter.jsx
@@ -1,340 +1,95 @@
 import { defineMessages, FormattedMessage } from 'react-intl';
 // Date.now().toString(36) 生成 时间戳 key
 const SALE_STRING = {
-    k5ddu8nr: {
-        id: 'SaleCenter.k5ddu8nr',
-        defaultMessage: '请选择店铺'
-    },
-    k5dlp2gl: {
-        id: 'SaleCenter.k5dlp2gl',
-        defaultMessage: '未开始'
-    },
-    k5dlp7zc: {
-        id: 'SaleCenter.k5dlp7zc',
-        defaultMessage: '执行中'
-    },
-    k5dlpczr: {
-        id: 'SaleCenter.k5dlpczr',
-        defaultMessage: '已结束'
-    },
-    k5eng042: {
-        id: 'SaleCenter.k5eng042',
-        defaultMessage: '全部',
-    },
-    k5eng7pt: {
-        id: 'SaleCenter.k5eng7pt',
-        defaultMessage: '置顶',
-    },
-    k5engebq: {
-        id: 'SaleCenter.k5engebq',
-        defaultMessage: '置底',
-    },
-    k5engk5b: {
-        id: 'SaleCenter.k5engk5b',
-        defaultMessage: '上移',
-    },
-    k5engpht: {
-        id: 'SaleCenter.k5engpht',
-        defaultMessage: '下移',
-    },
-    k5ey8jvj: {
-        id: 'SaleCenter.k5ey8jvj',
-        defaultMessage: '本页',
-    },
-    k5ey8l0e: {
-        id: 'SaleCenter.k5ey8l0e',
-        defaultMessage: '共',
-    },
-    k5ey8lip: {
-        id: 'SaleCenter.k5ey8lip',
-        defaultMessage: '条',
-    },
-    k5ez4ovx: {
-        id: 'SaleCenter.k5ez4ovx',
-        defaultMessage: '任意消费满'
-    },
-    k5ez4pdf: {
-        id: 'SaleCenter.k5ez4pdf',
-        defaultMessage: '任意消费每满'
-    },
-    k5ez4pvb: {
-        id: 'SaleCenter.k5ez4pvb',
-        defaultMessage: '指定菜品消费满'
-    },
-    k5ez4qew: {
-        id: 'SaleCenter.k5ez4qew',
-        defaultMessage: '指定菜品消费每满'
-    },
-    k5ez4qy4: {
-        id: 'SaleCenter.k5ez4qy4',
-        defaultMessage: '份'
-    },
-    k5ezcuto: {
-        id: 'SaleCenter.k5ezcuto',
-        defaultMessage: '减免金额'
-    },
-    k5ezcvbm: {
-        id: 'SaleCenter.k5ezcvbm',
-        defaultMessage: '特定售价'
-    },
-    k5ezdbiy: {
-        id: 'SaleCenter.k5ezdbiy',
-        defaultMessage: '元'
-    },
-    k5ezdc19: {
-        id: 'SaleCenter.k5ezdc19',
-        defaultMessage: '折'
-    },
-    k5ezdckg: {
-        id: 'SaleCenter.k5ezdckg',
-        defaultMessage: '例如8.8折'
-    },
-    k5f3y5ml: {
-        id: 'SaleCenter.k5f3y5ml',
-        defaultMessage: '张'
-    },
-    k5f3y6b4: {
-        id: 'SaleCenter.k5f3y6b4',
-        defaultMessage: '是'
-    },
-    k5f3y6yg: {
-        id: 'SaleCenter.k5f3y6yg',
-        defaultMessage: '否'
-    },
-    k5f3y7iv: {
-        id: 'SaleCenter.k5f3y7iv',
-        defaultMessage: '账单金额满'
-    },
-    k5f3y89j: {
-        id: 'SaleCenter.k5f3y89j',
-        defaultMessage: '账单金额每满'
-    },
+    k5ddu8nr: {id: 'Sale.k5ddu8nr',defaultMessage: '请选择店铺'},
+    k5dlp2gl: {id: 'Sale.k5dlp2gl',defaultMessage: '未开始'},
+    k5dlp7zc: {id: 'Sale.k5dlp7zc',defaultMessage: '执行中'},
+    k5dlpczr: {id: 'Sale.k5dlpczr',defaultMessage: '已结束'},
+    k5eng042: {id: 'Sale.k5eng042',defaultMessage: '全部'},
+    k5eng7pt: {id: 'Sale.k5eng7pt',defaultMessage: '置顶'},
+    k5engebq: {id: 'Sale.k5engebq',defaultMessage: '置底'},
+    k5engk5b: {id: 'Sale.k5engk5b',defaultMessage: '上移'},
+    k5engpht: {id: 'Sale.k5engpht',defaultMessage: '下移'},
+    k5ey8jvj: {id: 'Sale.k5ey8jvj',defaultMessage: '本页'},
+    k5ey8l0e: {id: 'Sale.k5ey8l0e',defaultMessage: '共'},
+    k5ey8lip: {id: 'Sale.k5ey8lip',defaultMessage: '条'},
+    k5ez4ovx: {id: 'Sale.k5ez4ovx',defaultMessage: '任意消费满'},
+    k5ez4pdf: {id: 'Sale.k5ez4pdf',defaultMessage: '任意消费每满'},
+    k5ez4pvb: {id: 'Sale.k5ez4pvb',defaultMessage: '指定菜品消费满'},
+    k5ez4qew: {id: 'Sale.k5ez4qew',defaultMessage: '指定菜品消费每满'},
+    k5ez4qy4: {id: 'Sale.k5ez4qy4',defaultMessage: '份'},
+    k5ezcuto: {id: 'Sale.k5ezcuto',defaultMessage: '减免金额'},
+    k5ezcvbm: {id: 'Sale.k5ezcvbm',defaultMessage: '特定售价'},
+    k5ezdbiy: {id: 'Sale.k5ezdbiy',defaultMessage: '元'},
+    k5ezdc19: {id: 'Sale.k5ezdc19',defaultMessage: '折'},
+    k5ezdckg: {id: 'Sale.k5ezdckg',defaultMessage: '例如8.8折'},
+    k5f3y5ml: {id: 'Sale.k5f3y5ml',defaultMessage: '张'},
+    k5f3y6b4: {id: 'Sale.k5f3y6b4',defaultMessage: '是'},
+    k5f3y6yg: {id: 'Sale.k5f3y6yg',defaultMessage: '否'},
+    k5f3y7iv: {id: 'Sale.k5f3y7iv',defaultMessage: '账单金额满'},
+    k5f3y89j: {id: 'Sale.k5f3y89j',defaultMessage: '账单金额每满'},
+    k5g5bcic: {id: "Sale.k5g5bcic",defaultMessage: "下单即赠送"},
+    k5g5bcqo: {id: "Sale.k5g5bcqo",defaultMessage: ""},
+    k5g5bcz0: {id: "Sale.k5g5bcz0",defaultMessage: ""},
+    k5g5bd7c: {id: "Sale.k5g5bd7c",defaultMessage: ""},
 };
 let COMMON_LABEL = defineMessages({
-    k5dbdped: {
-        id: 'SaleCenter.k5dbdped',
-        defaultMessage: '线上营销信息'
-    },
-    k5dbefat: {
-        id: 'SaleCenter.k5dbefat',
-        defaultMessage: '基本营销信息'
-    },
-    k5dbiuws: {
-        id: 'SaleCenter.k5dbiuws',
-        defaultMessage: '自动执行'
-    },
-    k5dk4m5r: {
-        id: 'SaleCenter.k5dk4m5r',
-        defaultMessage: '活动时间'
-    },
-    k5dlbwqo: {
-        id: 'SaleCenter.k5dlbwqo',
-        defaultMessage: '使用状态'
-    },
-    k5dlcm1i: {
-        id: 'SaleCenter.k5dlcm1i',
-        defaultMessage: '活动名称'
-    },
-    k5dk5uwl: {
-        id: 'SaleCenter.k5dk5uwl',
-        defaultMessage: '活动类型'
-    },
-    k5dldshc: {
-        id: 'SaleCenter.k5dldshc',
-        defaultMessage: '高级查询'
-    },
-    k5dlggak: {
-        id: 'SaleCenter.k5dlggak',
-        defaultMessage: '适用店铺'
-    },
-    k5dli0fu: {
-        id: 'SaleCenter.k5dli0fu',
-        defaultMessage: '有效状态'
-    },
-    k5dljb1v: {
-        id: 'SaleCenter.k5dljb1v',
-        defaultMessage: '统计类别'
-    },
-    k5dlpi06: {
-        id: 'SaleCenter.k5dlpi06',
-        defaultMessage: '标签'
-    },
-    k5dlpn4t: {
-        id: 'SaleCenter.k5dlpn4t',
-        defaultMessage: '品牌'
-    },
-    k5dlpt47: {
-        id: 'SaleCenter.k5dlpt47',
-        defaultMessage: '适用业务'
-    },
-    k5dml2ik: {
-        id: 'SaleCenter.k5dml2ik',
-        defaultMessage: '有效时间'
-    },
-    k5dmmiar: {
-        id: 'SaleCenter.k5dmmiar',
-        defaultMessage: '活动编码'
-    },
-    k5dmps71: {
-        id: 'SaleCenter.k5dmps71',
-        defaultMessage: '创建人/修改人'
-    },
-    k5dmrraa: {
-        id: 'SaleCenter.k5dmrraa',
-        defaultMessage: '创建时间/修改时间'
-    },
-    k5dmw1z4: {
-        id: 'SaleCenter.k5dmw1z4',
-        defaultMessage: '出错了，请稍后再试'
-    },
-    k5dn26n4: {
-        id: 'SaleCenter.k5dn26n4',
-        defaultMessage: '不限制'
-    },
-    k5dnw1q3: {
-        id: 'SaleCenter.k5dnw1q3',
-        defaultMessage: '您确定要删除吗'
-    },
-    k5do4z54: {
-        id: 'SaleCenter.k5do4z54',
-        defaultMessage: '删除数据是不可恢复操作, 请慎重考虑'
-    },
-    k5do0ps6: {
-        id: 'SaleCenter.k5do0ps6',
-        defaultMessage: '执行成功'
-    },
-    k5do6vse: {
-        id: 'SaleCenter.k5do6vse',
-        defaultMessage: '您将删除'
-    },
-    k5doarw8: {
-        id: 'SaleCenter.k5doarw8',
-        defaultMessage: '请求超时'
-    },
-    k5doax7i: {
-        id: 'SaleCenter.k5doax7i',
-        defaultMessage: '请求失败'
-    },
-    k5dod8s9: {
-        id: 'SaleCenter.k5dod8s9',
-        defaultMessage: '暂无数据'
-    },
-    k5dohc0d: {
-        id: 'SaleCenter.k5dohc0d',
-        defaultMessage: '更新活动信息'
-    },
-    k5ez4n7x: {
-        id: 'SaleCenter.k5ez4n7x',
-        defaultMessage: '活动方式'
-    },
-    k5ez4nw2: {
-        id: 'SaleCenter.k5ez4nw2',
-        defaultMessage: '消费一定的菜品，可对价格最低菜品进行减免、折扣或特定售价的优惠活动'
-    },
-    k5ez4odk: {
-        id: 'SaleCenter.k5ez4odk',
-        defaultMessage: '份数为'
-    },
-    k5ez4rmr: {
-        id: 'SaleCenter.k5ez4rmr',
-        defaultMessage: '金额不得为空'
-    },
-    k5ezcavr: {
-        id: 'SaleCenter.k5ezcavr',
-        defaultMessage: '折扣要大于0, 小于等于10'
-    },
-    k5ezccbt: {
-        id: 'SaleCenter.k5ezccbt',
-        defaultMessage: '特定售价'
-    },
-    k5ezcd0f: {
-        id: 'SaleCenter.k5ezcd0f',
-        defaultMessage: '减免'
-    },
-    k5ezcu1b: {
-        id: 'SaleCenter.k5ezcu1b',
-        defaultMessage: '折扣'
-    },
-    k5ezdwpv: {
-        id: 'SaleCenter.k5ezdwpv',
-        defaultMessage: '更多活动用户限制和互斥限制请使用'
-    },
-    k5ezdx9f: {
-        id: 'SaleCenter.k5ezdx9f',
-        defaultMessage: '高级设置'
-    },
-    k5ezdxpr: {
-        id: 'SaleCenter.k5ezdxpr',
-        defaultMessage: '对最低价菜品'
-    },
-    k5ezdxpr: {
-        id: 'SaleCenter.k5ezdxpr',
-        defaultMessage: '对最低价菜品'
-    },
-    k5f2114y: {
-        id: 'SaleCenter.k5f2114y',
-        defaultMessage: '请选择需要自动执行的活动'
-    },
-    k5f211mg: {
-        id: 'SaleCenter.k5f211mg',
-        defaultMessage: '执行顺序'
-    },
-    k5f2124s: {
-        id: 'SaleCenter.k5f2124s',
-        defaultMessage: '活动执行设置'
-    },
-    k5f212mo: {
-        id: 'SaleCenter.k5f212mo',
-        defaultMessage: '设置自动执行后，在SaaS结账界面将会严格按您设置的执行顺序自动执行营销活动，不再需要手动选择，将减少营业员手动操作的步骤，方便结账更快进行'
-    },
-    k5f21352: {
-        id: 'SaleCenter.k5f21352',
-        defaultMessage: '对SaaS结账时可使用的活动（除团购活动外）您可以根据店铺情况设置让活动自动执行'
-    },
-    k5f213qb: {
-        id: 'SaleCenter.k5f213qb',
-        defaultMessage: '选择活动'
-    },
-    k5f49c4d: {
-        id: 'SaleCenter.k5f49c4d',
-        defaultMessage: '券购买金额'
-    },
-    k5f49i2k: {
-        id: 'SaleCenter.k5f49i2k',
-        defaultMessage: '券面金额'
-    },
-    k5f49nwg: {
-        id: 'SaleCenter.k5f49nwg',
-        defaultMessage: '券交易手续费'
-    },
-    k5f49wf8: {
-        id: 'SaleCenter.k5f49wf8',
-        defaultMessage: '选择核销后必须输入券交易手续费'
-    },
-    k5f4a4be: {
-        id: 'SaleCenter.k5f4a4be',
-        defaultMessage: '费用是否计入实收'
-    },
-    k5f4aaf0: {
-        id: 'SaleCenter.k5f4aaf0',
-        defaultMessage: '团购券使用'
-    },
-    k5f4ahaw: {
-        id: 'SaleCenter.k5f4ahaw',
-        defaultMessage: '最多使用团购券'
-    },
-    k5f4ao7n: {
-        id: 'SaleCenter.k5f4ao7n',
-        defaultMessage: '可使用一张, 最多使用'
-    },
-    k5f4avmq: {
-        id: 'SaleCenter.k5f4avmq',
-        defaultMessage: '最多使用999张'
-    },
-    k5f4b1b9: {
-        id: 'SaleCenter.k5f4b1b9',
-        defaultMessage: '必须大于0'
-    },
+    k5dbdped: {id: 'Sale.k5dbdped',defaultMessage: '线上营销信息'},
+    k5dbefat: {id: 'Sale.k5dbefat',defaultMessage: '基本营销信息'},
+    k5dbiuws: {id: 'Sale.k5dbiuws',defaultMessage: '自动执行'},
+    k5dk4m5r: {id: 'Sale.k5dk4m5r',defaultMessage: '活动时间'},
+    k5dlbwqo: {id: 'Sale.k5dlbwqo',defaultMessage: '使用状态'},
+    k5dlcm1i: {id: 'Sale.k5dlcm1i',defaultMessage: '活动名称'},
+    k5dk5uwl: {id: 'Sale.k5dk5uwl',defaultMessage: '活动类型'},
+    k5dldshc: {id: 'Sale.k5dldshc',defaultMessage: '高级查询'},
+    k5dlggak: {id: 'Sale.k5dlggak',defaultMessage: '适用店铺'},
+    k5dli0fu: {id: 'Sale.k5dli0fu',defaultMessage: '有效状态'},
+    k5dljb1v: {id: 'Sale.k5dljb1v',defaultMessage: '统计类别'},
+    k5dlpi06: {id: 'Sale.k5dlpi06',defaultMessage: '标签'},
+    k5dlpn4t: {id: 'Sale.k5dlpn4t',defaultMessage: '品牌'},
+    k5dlpt47: {id: 'Sale.k5dlpt47',defaultMessage: '适用业务'},
+    k5dml2ik: {id: 'Sale.k5dml2ik',defaultMessage: '有效时间'},
+    k5dmmiar: {id: 'Sale.k5dmmiar',defaultMessage: '活动编码'},
+    k5dmps71: {id: 'Sale.k5dmps71',defaultMessage: '创建人/修改人'},
+    k5dmrraa: {id: 'Sale.k5dmrraa',defaultMessage: '创建时间/修改时间'},
+    k5dmw1z4: {id: 'Sale.k5dmw1z4',defaultMessage: '出错了，请稍后再试'},
+    k5dn26n4: {id: 'Sale.k5dn26n4',defaultMessage: '不限制'},
+    k5dnw1q3: {id: 'Sale.k5dnw1q3',defaultMessage: '您确定要删除吗'},
+    k5do4z54: {id: 'Sale.k5do4z54',defaultMessage: '删除数据是不可恢复操作, 请慎重考虑'},
+    k5do0ps6: {id: 'Sale.k5do0ps6',defaultMessage: '执行成功'},
+    k5do6vse: {id: 'Sale.k5do6vse',defaultMessage: '您将删除'},
+    k5doarw8: {id: 'Sale.k5doarw8',defaultMessage: '请求超时'},
+    k5doax7i: {id: 'Sale.k5doax7i',defaultMessage: '请求失败'},
+    k5dod8s9: {id: 'Sale.k5dod8s9',defaultMessage: '暂无数据'},
+    k5dohc0d: {id: 'Sale.k5dohc0d',defaultMessage: '更新活动信息'},
+    k5ez4n7x: {id: 'Sale.k5ez4n7x',defaultMessage: '活动方式'},
+    k5ez4nw2: {id: 'Sale.k5ez4nw2',defaultMessage: '消费一定的菜品，可对价格最低菜品进行减免、折扣或特定售价的优惠活动'},
+    k5ez4odk: {id: 'Sale.k5ez4odk',defaultMessage: '份数为'},
+    k5ez4rmr: {id: 'Sale.k5ez4rmr',defaultMessage: '金额不得为空'},
+    k5ezcavr: {id: 'Sale.k5ezcavr',defaultMessage: '折扣要大于0, 小于等于10'},
+    k5ezccbt: {id: 'Sale.k5ezccbt',defaultMessage: '特定售价'},
+    k5ezcd0f: {id: 'Sale.k5ezcd0f',defaultMessage: '减免'},
+    k5ezcu1b: {id: 'Sale.k5ezcu1b',defaultMessage: '折扣'},
+    k5ezdwpv: {id: 'Sale.k5ezdwpv',defaultMessage: '更多活动用户限制和互斥限制请使用'},
+    k5ezdx9f: {id: 'Sale.k5ezdx9f',defaultMessage: '高级设置'},
+    k5ezdxpr: {id: 'Sale.k5ezdxpr',defaultMessage: '对最低价菜品'},
+    k5ezdxpr: {id: 'Sale.k5ezdxpr',defaultMessage: '对最低价菜品'},
+    k5f2114y: {id: 'Sale.k5f2114y',defaultMessage: '请选择需要自动执行的活动'},
+    k5f211mg: {id: 'Sale.k5f211mg',defaultMessage: '执行顺序'},
+    k5f2124s: {id: 'Sale.k5f2124s',defaultMessage: '活动执行设置'},
+    k5f212mo: {id: 'Sale.k5f212mo',defaultMessage: '设置自动执行后，在SaaS结账界面将会严格按您设置的执行顺序自动执行营销活动，不再需要手动选择，将减少营业员手动操作的步骤，方便结账更快进行'},
+    k5f21352: {id: 'Sale.k5f21352',defaultMessage: '对SaaS结账时可使用的活动（除团购活动外）您可以根据店铺情况设置让活动自动执行'},
+    k5f213qb: {id: 'Sale.k5f213qb',defaultMessage: '选择活动'},
+    k5f49c4d: {id: 'Sale.k5f49c4d',defaultMessage: '券购买金额'},
+    k5f49i2k: {id: 'Sale.k5f49i2k',defaultMessage: '券面金额'},
+    k5f49nwg: {id: 'Sale.k5f49nwg',defaultMessage: '券交易手续费'},
+    k5f49wf8: {id: 'Sale.k5f49wf8',defaultMessage: '选择核销后必须输入券交易手续费'},
+    k5f4a4be: {id: 'Sale.k5f4a4be',defaultMessage: '费用是否计入实收'},
+    k5f4aaf0: {id: 'Sale.k5f4aaf0',defaultMessage: '团购券使用'},
+    k5f4ahaw: {id: 'Sale.k5f4ahaw',defaultMessage: '最多使用团购券'},
+    k5f4ao7n: {id: 'Sale.k5f4ao7n',defaultMessage: '可使用一张, 最多使用'},
+    k5f4avmq: {id: 'Sale.k5f4avmq',defaultMessage: '最多使用999张'},
+    k5f4b1b9: {id: 'Sale.k5f4b1b9',defaultMessage: '必须大于0'},
 });
 
 for (const key in COMMON_LABEL) {
