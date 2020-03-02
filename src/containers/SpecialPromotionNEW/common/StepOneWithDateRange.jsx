315968043b821b5afeade990076361c79e688d5c
dangxiaorun <dangxiaorun@hualala.com>
2020-03-02 12:51:22 +0800

    [chg]

--- src/containers/SpecialPromotionNEW/common/StepOneWithDateRange.jsx
+++ src/containers/SpecialPromotionNEW/common/StepOneWithDateRange.jsx
@@ -68,6 +68,7 @@ const simpleOptionSmsGate = [ // 选项有2种
     '65',
     '67',
     '75',
+    '76',
 ];
 
 const ATSEnabledTypes = [ // advanced time settings enabled promotion types
@@ -919,23 +920,6 @@ class StepOneWithDateRange extends React.Component {
                                     </FormItem>
                                 </Col>
                             </Row> : null
-                    }
-                    {
-                        this.props.type == '76' && 
-                        <FormItem
-                            label="是否发送消息"
-                            className={styles.FormItemStyle}
-                            labelCol={{ span: 4 }}
-                            wrapperCol={{ span: 17 }}
-                        >
-                            <Select size="default"
-                                    value={`${this.state.smsGate}`}
-                                    onChange={this.handleSmsGateChange}
-                                    getPopupContainer={(node) => node.parentNode}
-                            >
-                                {SEND_MSG.map(item => (<Option value={`${item.value}`} key={`${item.value}`}>{item.label}</Option>))}
-                            </Select>
-                        </FormItem>  
                     }
                     {fullOptionSmsGate.includes(String(this.props.type))
                          ?
