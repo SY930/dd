fede792bdc5942d2813b4ebab9dd2a29d2c5ea05
龚法曾 <gongfazeng@hualala.com>
2020-04-07 11:39:48 +0800

    创建秒杀和拼团活动时页面显示异常

--- src/containers/WeChatMall/AddNew.jsx
+++ src/containers/WeChatMall/AddNew.jsx
@@ -131,7 +131,7 @@ class NewActivity extends React.Component {
                     }}>
                     <div style={{ paddingBottom: 30 }} className={selfStyle.flexContainer}>
                         {this.renderActivityButtons()}
-                    </div>   
+                    </div>
                     {this.state.modal1Visible ? this.renderModal() : null}
                 </Col>
             </Row>
@@ -162,10 +162,11 @@ class NewActivity extends React.Component {
 
     renderModal = () => {
         const promotionType = WECHAT_MALL_ACTIVITIES[this.state.index].title;
+        const title = <span>创建{promotionType}</span>;
         return (
             <Modal
                 wrapClassName="progressBarModal"
-                title={`创建${promotionType}`}
+                title={title}
                 maskClosable={false}
                 footer={false}
                 style={{
