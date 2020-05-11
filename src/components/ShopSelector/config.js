84f1fe3de372b071099f1c8812bbd9bb3e7a2aed
龚法曾 <gongfazeng@hualala.com>
2020-05-11 18:24:47 +0800

    礼品品牌过滤，礼品选择店铺的时候品牌、城市等顺序参考线上修改

--- src/components/ShopSelector/config.js
+++ src/components/ShopSelector/config.js
@@ -1,15 +1,10 @@
+import { SALE_LABEL } from 'i18n/common/salecenter';
+
+
 export const FILTERS = [{
-    name: 'citys',
-    key: 'cityID',
-    label: '城市',
-    valueKey: 'cityID',
-    labelKey: 'cityName',
-    callserver: 'getShopCities',
-    callparams: {},
-}, {
     name: 'brands',
     key: 'brandID',
-    label: '品牌',
+    label: <span>{SALE_LABEL.k5dlpn4t}</span>,
     valueKey: 'brandID',
     labelKey: 'brandName',
     callserver: 'getShopBrand',
@@ -17,28 +12,42 @@ export const FILTERS = [{
 }, {
     name: 'shopCategories',
     key: 'shopCategoryID',
-    label: '门店组',
+    label: <span>{SALE_LABEL.d16hlk3kccf0161}</span>,
     valueKey: 'shopCategoryID',
     labelKey: 'shopCategoryName',
     callserver: 'getShopBrand',
     callparams: {},
 }, {
+    name: 'businessModels',
+    key: 'businessModel',
+    label: <span>{SALE_LABEL.dok9lk8l8157}</span>,
+    valueKey: 'businessModel',
+    labelKey: 'businessType',
+}, /*{
     name: 'orgs',
     key: 'parentOrgID',
     label: '组织',
     display: 'tree',
     valueKey: 'orgID',
     labelKey: 'orgName',
-}, {
-    name: 'shopTags',
-    key: 'tagIDs',
-    label: '店铺属性',
-    display: 'treeLeaf',
-    valueKey: 'tagID',
-    labelKey: 'tagName',
+},*/ {
+    name: 'citys',
+    key: 'cityID',
+    label: <span>{SALE_LABEL.dok9lk8l82132}</span>,
+    valueKey: 'cityID',
+    labelKey: 'cityName',
+    callserver: 'getShopCities',
+    callparams: {},
 }, {
     name: 'orgTags',
     key: 'orgTagIDs',
-    label: '管理组织',
+    label: <span>{SALE_LABEL.d5g4obe6e1346}</span>,
     display: 'tree',
+}, {
+    name: 'shopTags',
+    key: 'tagIDs',
+    label: <span>{SALE_LABEL.d455j75nqf4152}</span>,
+    display: 'treeLeaf',
+    valueKey: 'tagID',
+    labelKey: 'tagName',
 }];
