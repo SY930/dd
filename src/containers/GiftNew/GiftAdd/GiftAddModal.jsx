038df23e2de5327195ecafb33ce657710e0dff58
ranshaw <493060037@qq.com>
2020-07-03 18:53:56 +0800

    feat(定额礼品卡): 新增券包选择功能

--- src/containers/GiftNew/GiftAdd/GiftAddModal.jsx
+++ src/containers/GiftNew/GiftAdd/GiftAddModal.jsx
@@ -73,6 +73,7 @@ class GiftAddModal extends React.Component {
         } catch (e) {
             // oops
         }
+
     }
     componentWillReceiveProps(nextProps) {
         if (nextProps.shopSchema.getIn(['shopSchema']) !== this.props.shopSchema.getIn(['shopSchema'])) {
@@ -129,6 +130,15 @@ class GiftAddModal extends React.Component {
         if(key==='valueType') {
             this.setState({ valueType: value });
         }
+        if(key === 'quotaCardGiftConfList') {
+            const quotaCardGiftConfList = +this.baseForm.getFieldValue('quotaCardGiftConfList') || {};
+            if(!Object.keys(quotaCardGiftConfList).includes('presentType') ) {
+                // 初始化礼品详情的值
+                this.initGiftDetail()
+            }
+        }
+
+
     }
     handleSubmit() {
         const { groupTypes } = this.state;
@@ -138,6 +148,26 @@ class GiftAddModal extends React.Component {
             let params = _.assign(values, { giftType: value });
             let callServer = '';
             let shopNames = '', shopIDs = '';
+            const presentType = params.quotaCardGiftConfList.presentType
+            if(presentType == 1) {
+                params = {
+                    ...params,
+                    ...params.quotaCardGiftConfList
+                }
+            } else if(presentType == 4) {
+                params = {
+                    ...params,
+                    presentType,
+                    quotaCardGiftConfList: params.quotaCardGiftConfList.chooseCoupon
+                }
+            }  else if(presentType == 0) {
+                params = {
+                    ...params,
+                    presentType,
+                    quotaCardGiftConfList: []
+                }
+            }
+
             try {
                 if (params.shopNames) {
                     const shops = this.state.shopSchema.shops;
@@ -230,6 +260,34 @@ class GiftAddModal extends React.Component {
     }
     handleCurrencyChange = (currency) => {
         this.setState({ currency });
+    }
+    initGiftDetail = () => {
+        // 后端定义神奇的接口，为券包的时候，入参数，放quotaCardGiftConfList，从couponPackageBaseInfo取，入参和出参不一致
+        const  { quotaCardGiftConfList, presentType = 1, couponPackageBaseInfo } = this.props.gift.data
+        let params = {
+            presentType,
+            quotaCardGiftConfList: [],
+            chooseCoupon: []
+        }
+
+        if(presentType === 4 && couponPackageBaseInfo) {
+            params = {
+                presentType,
+                chooseCoupon: [couponPackageBaseInfo],
+                quotaCardGiftConfList: []
+            }
+        }
+        if(presentType === 1) {
+            params = {
+                presentType,
+                quotaCardGiftConfList,
+                chooseCoupon: []
+            }
+        }
+
+        this.baseForm.setFieldsValue({
+            quotaCardGiftConfList: params
+        });
     }
     render() {
         const { gift: { name: describe, value, data }, visible, type, treeData } = this.props;
@@ -563,7 +621,7 @@ class GiftAddModal extends React.Component {
                 type: 'custom',
                 label: '礼品详情',
                 defaultValue: [],
-                render: d => d()(<GiftInfo />),
+                render: d => d()(<GiftInfo groupID={this.props.accountInfo.toJS().groupID} />),
             },
         };
         const { valueType, monetaryUnit } = this.state;
--- src/containers/GiftNew/GiftAdd/GiftInfo.jsx
+++ src/containers/GiftNew/GiftAdd/GiftInfo.jsx
@@ -5,27 +5,55 @@ import GiftModal from './GiftModal';
 import { getCardList } from './AxiosFactory';
 import { SALE_CENTER_GIFT_EFFICT_TIME, SALE_CENTER_GIFT_EFFICT_DAY } from '../../../redux/actions/saleCenterNEW/types';
 import GiftCfg from '../../../constants/Gift';
+import { getVersionUI } from 'utils'
+import AddCouponModal from './coupon/AddModal'
+import {   typeMap } from './coupon/Common';
 
 const href = 'javascript:;';
 const DF = 'YYYYMMDD';
+const ButtonGroup = Button.Group;
+const btnList = [
+    {
+        value: '1',
+        label: '优惠券'
+    },
+    {
+        value: '4',
+        label: '券包'
+    },
+    {
+        value: '0',
+        label: '不赠送'
+    }
+]
 /** 任意金额组件 */
 export default class GiftInfo extends Component {
     state = {
         visible: false,      // 弹层是否显示
         giftTreeData: [],
+        currentChoose: 0,
+        showCouponModal: false,
     }
     componentDidMount() {
         getCardList({}).then(x => {
             this.setState({ giftTreeData: x });
         });
     }
+    handelEdit = (record,v) => () => {
+
+        this.setState({
+            visible: true,
+        })
+    }
     /* 生成表格头数据 */
     generateColumns() {
         const { disabled } = this.props;
         const { tc } = styles;
-        const render = (v) => {
+        const render = (v,record) => {
             return (
-                <a href={href} name={v} disabled={disabled} onClick={this.onDelete}>删除</a>
+                <div>
+                    <a href={href} name={v} disabled={disabled} onClick={this.onDelete}>删除</a>
+                </div>
             );
         };
         const render1 = (v,o) => {
@@ -51,7 +79,7 @@ export default class GiftInfo extends Component {
         };
         // 表格头部的固定数据
         return [
-            { width: 40, title: '序号', dataIndex: 'idx', className: tc },
+            { width: 0, title: '序号', dataIndex: 'idx', className: tc },
             { width: 100, title: '礼品类型', dataIndex: 'giftType', className: tc, render: render3 },
             { width: 100, title: '礼品名称', dataIndex: 'giftName', className: tc, render: render4, },
             { width: 100, title: '礼品金额(元)', dataIndex: 'giftValue', className: tc },
@@ -63,7 +91,8 @@ export default class GiftInfo extends Component {
     /* 生成表格数据 */
     generateDataSource() {
         const { value } = this.props;
-        return value.map((x, i) => ({
+        const { quotaCardGiftConfList = [] } = value
+        return quotaCardGiftConfList.map((x, i) => ({
             key: x.giftItemID + i,
             idx: i + 1,
             index: i,
@@ -72,7 +101,7 @@ export default class GiftInfo extends Component {
     }
     /* 关闭模态框 */
     toggleModal = () => {
-        this.setState(ps => ({ visible: !ps.visible }));
+        this.setState(ps => ({ visible: !ps.visible,isEdit: false }));
     }
     /** 增加 */
     onPost = (params) => {
@@ -100,33 +129,159 @@ export default class GiftInfo extends Component {
                 obj = { giftType, giftTypeName, giftItemID, giftName, giftValue };
             }
         });
-        const list = [...value, { ...params, ...obj, ...date, effectType: newEffectType }];
-        onChange(list);
+        const list = [...value.quotaCardGiftConfList, { ...params, ...obj, ...date, effectType: newEffectType }];
+        onChange({
+            ...value,
+            presentType: value.presentType,
+            quotaCardGiftConfList: list,
+        });
     }
     /** 删除 */
     onDelete = ({ target }) => {
         const { name: index } = target;
         const { value, onChange } = this.props;
-        const list = [...value];
+        const list = [...value.quotaCardGiftConfList];
         list.splice(index, 1);
-        onChange(list);
+        onChange({
+            ...value,
+            presentType: value.presentType,
+            quotaCardGiftConfList: list,
+        });
+    }
+    handleClickBtn = (i) => () => {
+        const { onChange,value } = this.props;
+        onChange({
+            ...value,
+            presentType: Number(btnList[i].value)
+        });
+    }
+    handleShowCoupon = () => {
+        this.setState({
+            showCouponModal: true
+        })
+    }
+
+    onSelectBag = (item) => {
+        const { onChange, value } = this.props;
+        onChange({
+            ...value,
+            chooseCoupon:  [item],
+            presentType: value.presentType
+        });
+        this.handleCloseCoupon()
+    }
+
+    handleCloseCoupon = () => {
+        this.setState({
+            showCouponModal: false
+        });
+    }
+    handleDeleteCoupon = () => {
+        const { onChange,value } = this.props;
+        onChange({
+            ...value,
+            chooseCoupon:  [],
+            presentType: value.presentType
+        });
+    }
+     /* 生成表格头数据 */
+     generateCouponColumns() {
+        const { tc } = styles;
+        const render = (v, o) => {
+            return (<a href={href} onClick={this.handleDeleteCoupon}>删除</a>);
+        };
+        const render1 = (v, o) => {
+            return (<Tooltip title={v}>
+                    <span>{v}</span>
+                </Tooltip>);
+        };
+        const render2 = (v, o) => {
+            const {sellBeginTime, sellEndTime } = o;
+            let text = sellBeginTime + ' ~ ' + sellEndTime;
+            if(sellBeginTime==='0'){
+                text = '长期有效';
+            }
+            return (<span>{text}</span>);
+        };
+        const render3 = (v, o) => {
+            const val = (v === -1) ? '不限制' : v;
+            return (<span>{val}</span>);
+        };
+        // 表格头部的固定数据
+        return [
+            { title: '券包名称', dataIndex: 'couponPackageName', render: render1 },
+            { width: 100, title: '券包类型', dataIndex: 'type' },
+            { width: 100, title: '库存', dataIndex: 'couponPackageStock', render: render3, className: tc },
+            { width: 200, title: '有效期', dataIndex: 'postTime', className: tc, render: render2 },
+            { width: 80, title: '操作', dataIndex: 'op', className: tc, render },
+        ];
+    }
+      /* 生成表格数据 */
+      generateCouponDataSource() {
+        const { value } = this.props;
+        const { chooseCoupon = [] } = value
+        return chooseCoupon.map((x, i) => ({
+            key: x.couponPackageID,
+            type: typeMap[x.couponPackageType],
+            ...x,
+        }));
     }
     render() {
-        const { visible, giftTreeData } = this.state;
+        const { visible, giftTreeData,showCouponModal,chooseCoupon } = this.state;
         const { value, disabled } = this.props;
         const columns = this.generateColumns();
         const dataSource = this.generateDataSource();
         //礼品定额卡添加优惠券限制最多10种
+        let actStyle = {
+            color: '#1ab495',
+            borderColor: '#1ab495'
+        }
+        let actBtnRight = {
+            borderLeftColor: '#1ab495'
+        }
+        if(getVersionUI() && getVersionUI().saasServiceVersionInfo === '2') {
+            actStyle = {
+                color: '#108ee9',
+                borderColor: '#108ee9'
+            }
+            actBtnRight = {
+                borderLeftColor: '#108ee9'
+            }
+        }
+        const couponColumns = this.generateCouponColumns();
+        const couponDataSource = this.generateCouponDataSource();
+        const currentChoose = btnList.findIndex(v => v.value === String(value.presentType))
+
         return (
             <div className={styles.cGiftInfo}>
-                {!value[9] && <Button icon="plus" disabled={disabled} onClick={this.toggleModal}>添加礼品</Button>}
-                <Table
+                <div style={{marginBottom: '10px'}}>
+                    <ButtonGroup>
+                        {btnList.map((v,i) => {
+                            return <Button style={currentChoose === i ? actStyle : (currentChoose + 1) === i ? actBtnRight : {}  } onClick={this.handleClickBtn(i)} key={i}>{v.label}</Button>
+                        })}
+                    </ButtonGroup>
+                </div>
+
+                {!value[9] && currentChoose === 0 && <Button icon="plus" disabled={disabled} onClick={this.toggleModal}>添加优惠券</Button>}
+                {currentChoose === 1 && <Button icon="plus" disabled={value.chooseCoupon && value.chooseCoupon.length > 0 || disabled} onClick={this.handleShowCoupon}>添加券包</Button>}
+                {currentChoose === 1 &&  <Table
+                    bordered={true}
+                    columns={couponColumns}
+                    dataSource={couponDataSource}
+                    style={{ maxWidth: 1000 }}
+                    pagination={false}
+                />}
+                {currentChoose === 0 && <Table
                     bordered={true}
                     columns={columns}
                     dataSource={dataSource}
                     pagination={false}
-                />
-                {visible &&
+                />}
+                {showCouponModal && <AddCouponModal
+                        groupID={this.props.groupID}
+                        onAdd={this.onSelectBag}
+                        onClose={this.handleCloseCoupon}/>}
+                {visible   &&
                     <GiftModal
                         treeData={giftTreeData}
                         onClose={this.toggleModal}
