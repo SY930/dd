ed9298e0fb53f73c301a1a69037792fd13682838
dangxiaorun <dangxiaorun@hualala.com>
2021-02-03 10:57:57 +0800

    【批量编辑共享组】当共享组超过100个时，弹窗提示信息需要和需求文档一致

--- src/containers/PromotionDecoration/CommentSendGiftDecorationBoard.jsx
+++ src/containers/PromotionDecoration/CommentSendGiftDecorationBoard.jsx
@@ -13,7 +13,7 @@ import {
 import tagImg from './assets/tag.svg'
 import { COMMON_LABEL, COMMON_STRING } from 'i18n/common';
 import { SALE_LABEL, SALE_STRING } from 'i18n/common/salecenter';
-import {injectIntl} from './IntlDecor';
+import { injectIntl } from './IntlDecor';
 
 
 const { TabPane } = Tabs;
@@ -35,16 +35,16 @@ export default class CommentSendGiftDecorationBoard extends Component {
         } = this.props;
         return (
             <div className={style.previewArea}>
-                <img src={iphone} alt=""/>
+                <img src={iphone} alt="" />
                 {
                     tabKey === '2' ? (
                         <div className={style.simpleDisplayBlock}>
                             <div className={style.imgWrapper}>
-                                <div style={{ width: '100%', height: '100%',  overflow: 'hidden' }}>
-                                    <img src={endImg} style={{ width: '100%' }} alt=""/>
+                                <div style={{ width: '100%', height: '100%', overflow: 'hidden' }}>
+                                    <img src={endImg} style={{ width: '100%' }} alt="" />
                                 </div>
                                 <div className={style.tagWrapper}>
-                                    <img src={tagImg} alt=""/>
+                                    <img src={tagImg} alt="" />
                                     <span>{SALE_LABEL.k6346c3s}</span>
                                 </div>
                             </div>
@@ -53,20 +53,20 @@ export default class CommentSendGiftDecorationBoard extends Component {
                                 <img src={giftExample} alt="" />
                                 <img src={giftExample} alt="" />
                             </div>
-                            <Icon className={style.closeBtn}  type="close-circle-o" />
+                            <Icon className={style.closeBtn} type="close-circle-o" />
                         </div>
                     ) : (
-                        <div className={style.simpleDisplayBlock}>
-                            <div className={style.tagWrapper}>
-                                <img src={tagImg} alt=""/>
-                                <span>{SALE_LABEL.k6346c3s}</span>
+                            <div className={style.simpleDisplayBlock}>
+                                <div className={style.tagWrapper}>
+                                    <img src={tagImg} alt="" />
+                                    <span>{SALE_LABEL.k6346c3s}</span>
+                                </div>
+                                <div style={{ borderRadius: 10, width: '100%', height: '100%', overflow: 'hidden' }}>
+                                    <img src={enterImg} style={{ width: '100%' }} alt="" />
+                                </div>
+                                <Icon className={style.closeBtn} type="close-circle-o" />
                             </div>
-                            <div style={{ borderRadius: 10, width: '100%', height: '100%', overflow: 'hidden' }}>
-                                <img src={enterImg} style={{ width: '100%' }} alt=""/>
-                            </div>
-                            <Icon className={style.closeBtn}  type="close-circle-o" />
-                        </div>
-                    )
+                        )
                 }
             </div>
         )
@@ -83,7 +83,7 @@ export default class CommentSendGiftDecorationBoard extends Component {
             <div>
                 <div className={style.sectionWrapper}>
                     <div className={style.label}>{SALE_LABEL.k6346cc4}</div>
-                    <ColorSettingBlock value={endColor} onChange={(value) => onChange({key: ['endColor'], value})} />
+                    <ColorSettingBlock value={endColor} onChange={(value) => onChange({ key: ['endColor'], value })} />
                 </div>
                 <div className={style.sectionWrapper}>
                     <div style={{ top: 30 }} className={style.label}>{SALE_LABEL.k6346c3s}</div>
@@ -94,7 +94,7 @@ export default class CommentSendGiftDecorationBoard extends Component {
                             value={endImg}
                             cropperRatio={920 / 360}
                             width={245}
-                            onChange={value => onChange({key: ['endImg'], value})}
+                            onChange={value => onChange({ key: ['endImg'], value })}
                         />
                         <div className={style.uploaderTip}>
                             <p>* {SALE_LABEL.k6346ckg}1000KB</p>
@@ -103,7 +103,7 @@ export default class CommentSendGiftDecorationBoard extends Component {
                         </div>
                     </div>
                 </div>
-        <Button style={{ marginLeft: 150 }} type="primary" onClick={() => this.setState({tabKey: '1'})}>{SALE_LABEL.k635s2yp}</Button>
+                <Button style={{ marginLeft: 150 }} type="primary" onClick={() => this.setState({ tabKey: '1' })}>{SALE_LABEL.k635s2yp}</Button>
             </div>
 
         )
@@ -121,18 +121,18 @@ export default class CommentSendGiftDecorationBoard extends Component {
             <div className={style.boardWrapper}>
                 {this.renderPhonePreview()}
                 <div>
-                <Tabs activeKey={this.state.tabKey} onTabClick={(tabKey) => this.setState({tabKey})} className={style.customTabWrapper}>
+                    <Tabs activeKey={this.state.tabKey} onTabClick={(tabKey) => this.setState({ tabKey })} className={style.customTabWrapper}>
                         <TabPane tab={k635s3fd + '1'} key="1">
                             <div style={{ paddingTop: 45 }}>
                                 <div className={style.sectionWrapper}>
-        <div style={{ top: 30 }} className={style.label}>{SALE_LABEL.k6346c3s}</div>
+                                    <div style={{ top: 30 }} className={style.label}>{SALE_LABEL.k6346c3s}</div>
                                     <div style={{ width: 350 }} className={style.uploaderWrapper}>
                                         <CropperUploader
                                             isAbsoluteUrl={true}
                                             cropperRatio={920 / 1346}
                                             limit={1000}
                                             value={enterImg}
-                                            onChange={value => onChange({key: ['enterImg'], value})}
+                                            onChange={value => onChange({ key: ['enterImg'], value })}
                                         />
                                         <div className={style.uploaderTip}>
                                             <p>* {SALE_LABEL.k6346ckg}1000KB</p>
@@ -141,7 +141,7 @@ export default class CommentSendGiftDecorationBoard extends Component {
                                         </div>
                                     </div>
                                 </div>
-        <Button style={{ marginLeft: 150 }} type="primary" onClick={() => this.setState({tabKey: '2'})}>{SALE_LABEL.k635s371}</Button>
+                                <Button style={{ marginLeft: 150 }} type="primary" onClick={() => this.setState({ tabKey: '2' })}>{SALE_LABEL.k635s371}</Button>
                             </div>
                         </TabPane>
                         <TabPane tab={k635s3fd + '2'} key="2">
