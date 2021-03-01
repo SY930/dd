8bf2dc33d93e02f9d87579a4d3fe778c8eecbbe5
senlyn <568655461@qq.com>
2021-03-01 15:12:40 +0800

    【券包】券包使用数页面发出方式中“定额卡充值赠送”未显示出来

--- src/containers/GiftNew/GiftInfo/TicketBag/Detail/index.jsx
+++ src/containers/GiftNew/GiftInfo/TicketBag/Detail/index.jsx
@@ -232,8 +232,8 @@ class Detail extends Component {
                         </div>
                     </li>
                     <li className={styles.dataBox}>
-                        <h3>数据统计</h3>
-                        <Tabs defaultActiveKey="1" className="tabsStyles">
+                        <h3>数据统计1</h3>
+                        <Tabs defaultActiveKey="1" className="tabsStyles" style={{overflow:'auto'}}>
                             <TabPane tab="发出数" key="1">
                                 <Popover
                                     content={this.renderPopOver()}
