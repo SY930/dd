cc33b0725d2b06bfda5e26d834a7ad8836f7d68f
dangxiaorun <dangxiaorun@hualala.com>
2021-03-15 17:30:51 +0800

    【前端】营销盒子授权到期提示优化

--- src/containers/NewCreatePromotions/NewPromotionCard.jsx
+++ src/containers/NewCreatePromotions/NewPromotionCard.jsx
@@ -80,179 +80,181 @@ class NewPromotionCard extends Component {
     showExpire = (date) => {
         let com = new Date(date).getTime()
         let now = new Date().getTime()
-        // debugger
-        return com - now <= (1000 * 3600 * 24 * 30)
+        return {
+            ifShow: com - now < (1000 * 3600 * 24 * 31),
+            howMany: Math.ceil((com - now) / (1000 * 3600 * 24)),
+        }
     }
     renderPulgin(key, ath) {
-        // debugger
         const { whiteList = [], authPluginStatus } = this.props;
         const isUse = this.filterItem(key);
         if (pulgins.includes(key)) {
             const item = whiteList.find(x => x.eventWay == key);
             const { expireDate } = item || {};
             const date = moment(expireDate, 'YYYYMMDD').format('YYYY/MM/DD')
-            let text = authPulgins.includes(key) && !authPluginStatus ? '联系商务开通' : (isUse ? `授权期至:${date}` : '申请试用');
-            // 秒杀71  拼团72  默认永久开通  无text
-            let content = ['71', '72'].includes(key) ? '' : <em className={ath ? styles.validDateAth : styles.validDate}>{text}</em>
-            console.log(date)
-            if (this.showExpire(date)) {
+            let obj = this.showExpire(date)
+            // 秒杀71  拼团72  默认永久开通  无text redValidDate
+            if (obj.ifShow) {
+                let text = authPulgins.includes(key) && !authPluginStatus ? '联系商务开通' : (isUse ? `还有${obj.howMany}天到期` : '申请试用');
+                console.log(text, ath, key)
+                let content = ['71', '72'].includes(key) ? '' : <em className={ath ? styles.validDateAth : (authPulgins.includes(key) && !authPluginStatus) ? styles.validDate : (isUse ? styles.redValidDate : styles.validDate)}> { text }</em >
                 return content
-            }
-            return null
         }
+        return null
+    }
+}
+
+render() {
+    let {
+        promotionEntity: {
+            tags = [],
+            title,
+            text,
+            example,
+            key,
+            right = 0,
+            bottom = 0,
+            isNew,
+        },
+        index,
+        onCardClick,
+        size,
+        authPluginStatus
+    } = this.props;
+    let backgroundImageString;
+    switch (index % 4) {
+        case 1: backgroundImageString = bg1; break;
+        case 2: backgroundImageString = bg2; break;
+        case 3: backgroundImageString = bg3; break;
+        default: backgroundImageString = bg0;
     }
 
-    render() {
-        let {
-            promotionEntity: {
-                tags = [],
-                title,
-                text,
-                example,
-                key,
-                right = 0,
-                bottom = 0,
-                isNew,
-            },
-            index,
-            onCardClick,
-            size,
-            authPluginStatus
-        } = this.props;
-        let backgroundImageString;
-        switch (index % 4) {
-            case 1: backgroundImageString = bg1; break;
-            case 2: backgroundImageString = bg2; break;
-            case 3: backgroundImageString = bg3; break;
-            default: backgroundImageString = bg0;
-        }
-
-        if (size === 'small') {
-            return (
-                <div className={styles.smallContainer} onClick={this.onClick}>
-                    {this.renderPulgin(key, 'ath')}
-                    <div className={styles.title}>
-                        {title}
-                    </div>
-                    <div className={styles.cardBackgroundImage}>
-                        <img src={backgroundImageString} alt="oops" />
-                    </div>
-                    <div className={styles.cardLogo} style={{
-                        right: right * 0.62,
-                        bottom: bottom * 0.62,
-                    }}>
-                        {V3KEYS.includes(key) ?
-                            <img style={{ maxWidth: '90px', maxHeight: '90px' }} src={V3LOGO[key]} alt="oops" /> :
-                            <img style={{ maxWidth: '90px', maxHeight: '90px' }} src={require(`./assets/logo_${key}.png`)} alt="oops" />
-                        }
-                    </div>
-                </div>
-            )
-        }
-
-        // if(size === 'special') {
-        let wechatFlag = 1;
+    if (size === 'small') {
         return (
-            <div className={styles.speContainer} onClick={this.onClick} style={{ border: '1px solid #C4C4C480', }}>
-                <p className={styles.expandableP}>
-                    {isNew ? <span><img className={styles.xinImg} src={xin} /></span> : null}
-                    {this.renderPulgin(key)}
-                </p>
+            <div className={styles.smallContainer} onClick={this.onClick}>
+                {this.renderPulgin(key, 'ath')}
                 <div className={styles.title}>
                     {title}
-                    <div className={styles.speTag}>
-                        {tags.map((tag, i) => {
-                            if (!wechatFlag && tag.props && tag.props.defaultMessage.includes('微信') || !wechatFlag && !tag.props && tag.includes('微信')) {
-                                return null;
-                            }
-                            if (tag.props && tag.props.defaultMessage.includes('微信') || !tag.props && tag.includes('微信')) {
-                                wechatFlag--;
-                            }
-                            return (<div className={styles.speTagSpan} key={i}>{
-                                tag.props ?
-                                    tag.props.defaultMessage.includes('小程序') ?
-                                        <img className={styles.speTagImg} src={xcx} /> :
-                                        tag.props.defaultMessage.includes('微信') ?
-                                            <img className={styles.speTagImg} src={wx} /> :
-                                            tag.props.defaultMessage.includes('pos') ? <img className={styles.speTagImg} src={pos} /> :
-                                                <span><img className={styles.speTagImg} src={xcx} /><img className={styles.speTagImg} src={pos} /><img className={styles.speTagImg} src={wx} /></span>
-                                    : tag.includes('pos') ?
-                                        <img className={styles.speTagImg} src={pos} /> :
-                                        tag.includes('微信') ?
-                                            <img className={styles.speTagImg} src={wx} /> :
-                                            tag.includes('小程序') ? <img className={styles.speTagImg} src={xcx} /> : null
-                            }</div>)
-                        })}
-                    </div>
                 </div>
-                <Tooltip title={text}>
-                    <div className={styles.desDiv}>
-                        {text}
-                    </div>
-                    {/*<div className={styles.desDiv}>
-                        {example}
-                    </div>*/}
-                </Tooltip>
-
-                <div className={styles.speCardLogo} style={{
+                <div className={styles.cardBackgroundImage}>
+                    <img src={backgroundImageString} alt="oops" />
+                </div>
+                <div className={styles.cardLogo} style={{
                     right: right * 0.62,
                     bottom: bottom * 0.62,
                 }}>
                     {V3KEYS.includes(key) ?
-                        <img className={styles.speCardImg} src={V3LOGO[key]} alt="oops" /> :
-                        //商城活动 
-                        ['71', '72', '73'].includes(key) ?
-                            <img className={styles.speCardImg} src={require(`./assets/logo_${key}.png`)} alt="oops" /> :
-                            <img className={styles.speCardImg} src={require(`./assets/logo_${key}_new.png`)} alt="oops" />
+                        <img style={{ maxWidth: '90px', maxHeight: '90px' }} src={V3LOGO[key]} alt="oops" /> :
+                        <img style={{ maxWidth: '90px', maxHeight: '90px' }} src={require(`./assets/logo_${key}.png`)} alt="oops" />
                     }
                 </div>
             </div>
         )
-        // }
+    }
 
-        /* 新建营销活动老样式 */
-        /*return (
-            <div className={styles.container} onClick={this.onClick}>
+    // if(size === 'special') {
+    let wechatFlag = 1;
+    return (
+        <div className={styles.speContainer} onClick={this.onClick} style={{ border: '1px solid #C4C4C480', }}>
+            <p className={styles.expandableP}>
+                {isNew ? <span><img className={styles.xinImg} src={xin} /></span> : null}
                 {this.renderPulgin(key)}
-                <div className={styles.cardTitle}>
-                    {title}
-                </div>
-                {
-                    isNew && (
-                        <div className={styles.cardNewTag}>
-                            NEW
-                        </div>
-                    )
-                }
-                <div className={styles.tagsContainer}>
-                    {tags.map((tag, i) => (<div className={styles.tag} key={i}>{tag}</div>))}
-                </div>
-                <div
-                    className={styles.promotionDescription}
-                    style={{
-                        marginTop: 12
-                    }}
-                >
-                    {text}
-                </div>
-                <div className={styles.promotionDescription}>
-                    {example}
-                </div>
-                <div className={styles.cardBackgroundImage}>
-                    <img src={backgroundImageString} alt="oops"/>
-                </div>
-                <div className={styles.cardLogo} style={{
-                    right,
-                    bottom,
-                }}>
-                    {V3KEYS.includes(key) ?
-                        <img style={{maxWidth: '90px', maxHeight: '90px'}} src={V3LOGO[key]} alt="oops"/>:
-                        <img style={{  width: key == 75 ? 70 : key == 77 || key == 76 ? 58 : 'auto' }} src={require(`./assets/logo_${key}.png`)} alt="oops"/>
-                    }
+            </p>
+            <div className={styles.title}>
+                {title}
+                <div className={styles.speTag}>
+                    {tags.map((tag, i) => {
+                        if (!wechatFlag && tag.props && tag.props.defaultMessage.includes('微信') || !wechatFlag && !tag.props && tag.includes('微信')) {
+                            return null;
+                        }
+                        if (tag.props && tag.props.defaultMessage.includes('微信') || !tag.props && tag.includes('微信')) {
+                            wechatFlag--;
+                        }
+                        return (<div className={styles.speTagSpan} key={i}>{
+                            tag.props ?
+                                tag.props.defaultMessage.includes('小程序') ?
+                                    <img className={styles.speTagImg} src={xcx} /> :
+                                    tag.props.defaultMessage.includes('微信') ?
+                                        <img className={styles.speTagImg} src={wx} /> :
+                                        tag.props.defaultMessage.includes('pos') ? <img className={styles.speTagImg} src={pos} /> :
+                                            <span><img className={styles.speTagImg} src={xcx} /><img className={styles.speTagImg} src={pos} /><img className={styles.speTagImg} src={wx} /></span>
+                                : tag.includes('pos') ?
+                                    <img className={styles.speTagImg} src={pos} /> :
+                                    tag.includes('微信') ?
+                                        <img className={styles.speTagImg} src={wx} /> :
+                                        tag.includes('小程序') ? <img className={styles.speTagImg} src={xcx} /> : null
+                        }</div>)
+                    })}
                 </div>
             </div>
-        )*/
-    }
+            <Tooltip title={text}>
+                <div className={styles.desDiv}>
+                    {text}
+                </div>
+                {/*<div className={styles.desDiv}>
+                        {example}
+                    </div>*/}
+            </Tooltip>
+
+            <div className={styles.speCardLogo} style={{
+                right: right * 0.62,
+                bottom: bottom * 0.62,
+            }}>
+                {V3KEYS.includes(key) ?
+                    <img className={styles.speCardImg} src={V3LOGO[key]} alt="oops" /> :
+                    //商城活动 
+                    ['71', '72', '73'].includes(key) ?
+                        <img className={styles.speCardImg} src={require(`./assets/logo_${key}.png`)} alt="oops" /> :
+                        <img className={styles.speCardImg} src={require(`./assets/logo_${key}_new.png`)} alt="oops" />
+                }
+            </div>
+        </div>
+    )
+    // }
+
+    /* 新建营销活动老样式 */
+    /*return (
+        <div className={styles.container} onClick={this.onClick}>
+            {this.renderPulgin(key)}
+            <div className={styles.cardTitle}>
+                {title}
+            </div>
+            {
+                isNew && (
+                    <div className={styles.cardNewTag}>
+                        NEW
+                    </div>
+                )
+            }
+            <div className={styles.tagsContainer}>
+                {tags.map((tag, i) => (<div className={styles.tag} key={i}>{tag}</div>))}
+            </div>
+            <div
+                className={styles.promotionDescription}
+                style={{
+                    marginTop: 12
+                }}
+            >
+                {text}
+            </div>
+            <div className={styles.promotionDescription}>
+                {example}
+            </div>
+            <div className={styles.cardBackgroundImage}>
+                <img src={backgroundImageString} alt="oops"/>
+            </div>
+            <div className={styles.cardLogo} style={{
+                right,
+                bottom,
+            }}>
+                {V3KEYS.includes(key) ?
+                    <img style={{maxWidth: '90px', maxHeight: '90px'}} src={V3LOGO[key]} alt="oops"/>:
+                    <img style={{  width: key == 75 ? 70 : key == 77 || key == 76 ? 58 : 'auto' }} src={require(`./assets/logo_${key}.png`)} alt="oops"/>
+                }
+            </div>
+        </div>
+    )*/
+}
 }
 
 export default NewPromotionCard
