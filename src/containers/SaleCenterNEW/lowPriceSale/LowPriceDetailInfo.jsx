c8553ef7393963740d24aef0f036ba9c3d7a53ce
senlyn <568655461@qq.com>
2021-06-30 11:56:57 +0800

    支持测试公司

--- src/containers/SaleCenterNEW/lowPriceSale/LowPriceDetailInfo.jsx
+++ src/containers/SaleCenterNEW/lowPriceSale/LowPriceDetailInfo.jsx
@@ -240,7 +240,7 @@ class LowPriceDetailInfo extends React.Component {
         const { user:{groupID} } = this.props;
         const {subRule} = this.state;
         return (
-            groupID == '11157' ? 
+            groupID == '300452' || groupID == '11157' || groupID == '189702' ? 
                 <FormItem
                     label={'配菜是否参与计算'}
                     className={styles.FormItemStyle}
