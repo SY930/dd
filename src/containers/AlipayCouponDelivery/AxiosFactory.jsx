227904be79e5ef2da21fa7da1fdedc5fa7f23c1e
songnana <songnana@hualala.com>
2021-11-11 13:40:28 +0800

    init

--- src/containers/AlipayCouponDelivery/AxiosFactory.jsx
+++ src/containers/AlipayCouponDelivery/AxiosFactory.jsx
@@ -83,7 +83,7 @@ async function getShopPid() {
     const response = await axios.post(url + method, params);
     const { code, message: msg, data: obj } = response;
     if (code === '000') {
-        const { authPidDataList } = obj;
+        const { authPidDataList = [] } = obj;
         return authPidDataList
     }
     message.error(msg);
