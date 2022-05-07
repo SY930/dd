a374a58e533e860d7b55aa3cb0ebb94aa4c6a6ad
xinli <xinli@hualala.com>
2022-05-07 23:41:21 +0800

    从标签跳转

--- src/containers/NewCreatePromotions/NewCustomerPage.jsx
+++ src/containers/NewCreatePromotions/NewCustomerPage.jsx
@@ -312,6 +312,16 @@ class NewCustomerPage extends Component {
             this.props.saleCenterSetJumpOpenCardParams(false)
             // 新建逻辑
             this.clearUrl();
+        }else if (from === 'tagsSendGroupGift') { // 群发礼品
+            console.log('gohere1111111>>>>>>>>>>>>>>>>>>>>>>>>>>>>')
+            const item = REPEAT_PROMOTION_TYPES.filter((item) => {
+                return item.key == 53
+            })[0];
+            setTimeout(() => {
+                this.handleNewPromotionCardClick(item, true);
+            }, 1000)
+            // 新建逻辑
+            this.clearUrl();
         } else if (from === 'doNothingButSth') {
             const saleID = type;
             // this.setState({ currentCategoryIndex: 4 })
--- src/containers/NewCreatePromotions/NewOnSaleActivityPage.jsx
+++ src/containers/NewCreatePromotions/NewOnSaleActivityPage.jsx
@@ -297,6 +297,16 @@ class NewOnSaleActivityPage extends Component {
             this.props.saleCenterSetJumpOpenCardParams(false)
             // 新建逻辑
             this.clearUrl();
+        } else if (from === 'tagsSendGroupGift') { // 群发礼品
+            console.log('gohere22222>>>>>>>>>>>>>>>>>>>>>>>>>>>>')
+            const item = REPEAT_PROMOTION_TYPES.filter((item) => {
+                return item.key == 53
+            })[0];
+            setTimeout(() => {
+                this.handleNewPromotionCardClick(item, true);
+            }, 1000)
+            // 新建逻辑
+            this.clearUrl();
         } else if (from === 'doNothingButSth') {
             const saleID = type;
             // this.setState({ currentCategoryIndex: 4 })
