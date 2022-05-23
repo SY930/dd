66433c4f9f22d9f0e792a98d23b735b3c371ab65
songnana <songnana@hualala.com>
2022-05-23 18:03:10 +0800

    积分券选择小程序

--- src/containers/GiftNew/GiftAdd/GiftAddModal.jsx
+++ src/containers/GiftNew/GiftAdd/GiftAddModal.jsx
@@ -234,6 +234,7 @@ class GiftAddModal extends React.Component {
             params.openPushSms = params.pushMessage && params.pushMessage.sendType.indexOf('msg') !== -1 ? 1 : 0
             params.reminderTime = params.pushMessage && params.pushMessage.reminderTime
             params.pushMessageMpID = params.pushMessage && params.pushMessage.pushMessageMpID
+            params.pushMimiAppMsg = params.pushMessage && params.pushMessage.sendType.includes('mini') ? params.pushMessage.pushMimiAppMsg : null
             const { accountInfo, startSaving, endSaving } = this.props;
             const { groupName } = accountInfo.toJS();
             startSaving();
