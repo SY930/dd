74cc7208808def87cc973842e54f1875d645881c
donghangrui <donghangrui@hualala.com>
2022-06-22 10:47:39 +0800

    修改UI问题

--- src/containers/GiftNew/GiftInfo/GiftDetailTable.jsx
+++ src/containers/GiftNew/GiftInfo/GiftDetailTable.jsx
@@ -143,7 +143,7 @@ class GiftDetailTable extends Component {
     getTitle = () =>{  
         let title = []
         title.push(<div>排序 
-            <img style={{marginLeft:10,height:25,width:25}} src="https://res.hualala.com/basicdoc/36ac3db9-16d2-489c-9af3-4c1659b7b2a8.png"  onClick={()=>{
+            <img style={{marginLeft:10,height:14,width:14}} src="https://res.hualala.com/basicdoc/36ac3db9-16d2-489c-9af3-4c1659b7b2a8.png"  onClick={()=>{
                 let { tabkey } = this.props
                 Modal.confirm({
                     title: '重置排序',
