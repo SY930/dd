b9bf2c34b2b708b28f8c2669500ed0d1ce7579a4
xinli <xinli@hualala.com>
2022-07-06 17:59:06 +0800

    更改线上基础权限页面ID

--- src/constants/entryIds.jsx
+++ src/constants/entryIds.jsx
@@ -1,2 +1,3 @@
 //页面id,用来控制页面中各种操作权限，例如 编辑 删除 等，权限控制由entryId 和 rightCode 共同控制
-export const BASIC_PROMOTION_MANAGE_PAGE = '10000398'; // 基础营销管理页
+// export const BASIC_PROMOTION_MANAGE_PAGE = '10000398'; // 测试环境基础营销管理页
+export const BASIC_PROMOTION_MANAGE_PAGE = '10000922'; // 线上环境基础营销管理页
