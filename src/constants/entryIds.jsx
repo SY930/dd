36f78d555831e28f5bc0d786da3bb277c2aef74a
xinli <xinli@hualala.com>
2022-07-20 11:15:52 +0800

    entryIds 走配置

--- src/constants/entryIds.jsx
+++ src/constants/entryIds.jsx
@@ -1,9 +1,8 @@
 //页面id,用来控制页面中各种操作权限，例如 编辑 删除 等，权限控制由entryId 和 rightCode 共同控制
 
-// 测试环境
-export const BASIC_PROMOTION_MANAGE_PAGE = '10000398'; // 测试环境基础营销管理页
-export const SPECIAL_PROMOTION_MANAGE_PAGE = '10000400'; // 测试环境特色营销管理页
+let flag = HUALALA.ENVIRONMENT === 'production-release';//true 为线上环境
+console.log(flag,'flag================')
+export const BASIC_PROMOTION_MANAGE_PAGE = flag ? '10000922' : '10000398'; 
+export const SPECIAL_PROMOTION_MANAGE_PAGE = flag ? '10000932' : '10000400';
 
-// 线上环境
-// export const BASIC_PROMOTION_MANAGE_PAGE = '10000922'; // 线上环境基础营销管理页
-// export const SPECIAL_PROMOTION_MANAGE_PAGE = '10000932'; // 线上环境特色营销管理页
+
