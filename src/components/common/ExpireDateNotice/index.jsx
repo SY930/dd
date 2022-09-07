271409f2d3f4b9fd6ecc9bcca7f4ef90e9dd015f
xinli <xinli@hualala.com>
2022-09-07 15:41:17 +0800

    change verison 2.126.10

--- src/components/common/ExpireDateNotice/index.jsx
+++ src/components/common/ExpireDateNotice/index.jsx
@@ -12,7 +12,7 @@ class ExpireDateNotice extends React.Component {
         this.state = {
             showDate: null,
             isExpire: "noExpire",
-            visible: true,
+            visible: true
         };
     }
     componentDidMount() {
@@ -20,34 +20,25 @@ class ExpireDateNotice extends React.Component {
         this.getAuthLicenseData(productCode);
     }
     getAuthLicenseData = (productCode = "HLL_CRM_Marketingbox") => {
-        const { groupID } = getStore()
-            .getState()
-            .user.get("accountInfo")
-            .toJS();
+        const { groupID } = getStore().getState().user.get("accountInfo").toJS();
         axios
             .post("/api/v1/universal", {
                 service: "HTTP_SERVICE_URL_CRM",
                 type: "post",
                 data: {
                     groupID,
-                    productCode,
+                    productCode
                 },
-                method:
-                    "/crm/crmAuthLicenseService.queryCrmPluginLicenses.ajax",
+                method: "/crm/crmAuthLicenseService.queryCrmPluginLicenses.ajax"
             })
-            .then((ret) => {
+            .then(ret => {
                 if (ret.code === "000") {
                     let basicAuthEndDate = null;
                     if (productCode === "HLL_CRM_Marketingbox") {
                         let { plugins } = ret.data;
-                        basicAuthEndDate =
-                            plugins && plugins.length > 0
-                                ? plugins[0].authEndDate
-                                : null;
+                        basicAuthEndDate = plugins && plugins.length > 0 ? plugins[0].authEndDate : null;
                     } else {
-                        basicAuthEndDate = ret.data.basicAuthEndDate
-                            ? ret.data.basicAuthEndDate
-                            : null;
+                        basicAuthEndDate = ret.data.basicAuthEndDate ? ret.data.basicAuthEndDate : null;
                     }
                     let endDate = "";
                     let showDate = "";
@@ -69,42 +60,38 @@ class ExpireDateNotice extends React.Component {
                     if (disTime > 0 && disTime <= interval) {
                         this.setState({
                             showDate,
-                            isExpire: "near",
+                            isExpire: "near"
                         });
                     }
                     if (disTime < 0) {
                         this.setState({
-                            isExpire: "expired",
+                            isExpire: "expired"
                         });
                     }
                 } else {
                     this.setState({
-                        isExpire: "noExpired",
+                        isExpire: "noExpired"
                     });
                     message.warn(ret.message);
                 }
             });
     };
-    handleCloseNotice = (e) => {
+    handleCloseNotice = e => {
         this.setState({
-            visible: false,
+            visible: false
         });
     };
     render() {
         const { productCode, marginLeft = "", marginTop = "" } = this.props;
-        const { showDate, visible } = this.state;
+        const { isExpire, showDate, visible } = this.state;
         let insertText = "";
-        let isExpire = "expired";
         switch (isExpire) {
             case "near": //30天倒计时
                 if (productCode == "HLL_CRM_Marketingbox") {
                     insertText = (
                         <span className={styles.noticeText}>
                             营销大礼包将于
-                            <span style={{ color: "#dd576b" }}>
-                                {" "}
-                                {showDate}{" "}
-                            </span>
+                            <span style={{ color: "#dd576b" }}> {showDate} </span>
                             到期，请联系哗啦啦工作人员续费，以免影响使用，谢谢
                         </span>
                     );
@@ -112,11 +99,7 @@ class ExpireDateNotice extends React.Component {
                 break;
             case "expired": //已过期
                 if (productCode == "HLL_CRM_Marketingbox") {
-                    insertText = (
-                        <span className={styles.noticeText}>
-                            授权模块（营销大礼包）已到期，请尽快联系哗啦啦工作人员处理，以免影响使用，谢谢
-                        </span>
-                    );
+                    insertText = <span className={styles.noticeText}>授权模块（营销大礼包）已到期，请尽快联系哗啦啦工作人员处理，以免影响使用，谢谢</span>;
                 }
                 break;
             default:
@@ -124,11 +107,7 @@ class ExpireDateNotice extends React.Component {
                 break;
         }
         return isExpire === "noExpire" || !visible ? null : (
-            <Col
-                className={styles.expireDateNotice}
-                onClick={() => this.handleCloseNotice()}
-                style={{ marginLeft: marginLeft, marginTop: marginTop }}
-            >
+            <Col className={styles.expireDateNotice} onClick={() => this.handleCloseNotice()} style={{ marginLeft: marginLeft, marginTop: marginTop }}>
                 <Icon type="info-circle" className={styles.noticeInfoCircle} />
                 <Tooltip title={insertText}>{insertText}</Tooltip>
                 <Icon type="close" className={styles.noticeInfoClose} />
