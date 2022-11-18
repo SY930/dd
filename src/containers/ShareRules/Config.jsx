1ae0c316e08ab9831fc3cb54dc478bb7fa159f04
xinli <xinli@hualala.com>
2022-11-18 14:53:17 +0800

    农耕记开通500个共享互斥

--- src/containers/ShareRules/Config.jsx
+++ src/containers/ShareRules/Config.jsx
@@ -1,1 +1,10 @@
-export const ACCOUNTLIST = ['253686','292428','1348','318422','335612'];//这里的集团共享组数量放开 500个
+/*
+ * @Author: xinli xinli@hualala.com
+ * @Date: 2022-11-18 14:40:59
+ * @LastEditors: xinli xinli@hualala.com
+ * @LastEditTime: 2022-11-18 14:52:39
+ * @FilePath: /platform-sale/src/containers/ShareRules/Config.jsx
+ * @Description: 163832 添加农耕记集团
+ */
+
+export const ACCOUNTLIST = ['253686','292428','1348','318422','335612','163832'];//这里的集团共享组数量放开 500个
