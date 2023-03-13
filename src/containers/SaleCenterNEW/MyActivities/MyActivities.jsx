c3086969c864b80ec386c27bceee8b637edfd1ca
songnana <songnana@hualala.com>
2023-03-13 14:44:16 +0800

    修改状态后请求数据

--- src/containers/SaleCenterNEW/MyActivities/MyActivities.jsx
+++ src/containers/SaleCenterNEW/MyActivities/MyActivities.jsx
@@ -490,7 +490,10 @@ class MyActivities extends React.Component {
                 nextActive,
                 modalTip,
                 sale_promotionVersion: record ? record.promotionVersion : '1.0',
-                cb: () => {}
+                cb: (val) => {
+                    message.success(val);
+                    this.handleQuery(this.state.pageNo);
+                }
             });
         }
 
